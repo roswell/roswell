68134ff0f4c06a4deab52476664794e45268bb33
SANO Masatoshi <snmsts@gmail.com>
2014-09-27 15:03:02 +0900

    change commandline option style again.

--- src/lsp.c
+++ src/lsp.c
@@ -20,6 +20,7 @@ extern int cmd_version(int argc,char **argv);
 extern int cmd_opt(int argc,char **argv);
 extern int cmd_run(int argc,char **argv);
 
+//dummy
 int cmd_notyet(int argc,char **argv)
 {
   printf("notyet\n");
@@ -28,6 +29,7 @@ int cmd_notyet(int argc,char **argv)
 
 static struct sub_command commands[] = {
   /* longopt,shortopt,cmd_function,?, */
+  { " usage",NULL,cmd_notyet,0,1},
   { "help","h?",cmd_notyet,0,1},
   { "list",NULL,cmd_list,1,1},
   { "version",NULL,cmd_version,0,1},
@@ -39,15 +41,20 @@ static struct sub_command commands[] = {
   { "download",NULL,cmd_download,0,1},
 };
 
+static struct sub_command options[] = {
+  /* longopt,shortopt,cmd_function,?, */
+  { " dummy",NULL,cmd_notyet,0,1},
+};
+
 int proccmd(int argc,char** argv) {
   int i;
   int pos;
   if(argv[0][0]=='-') {
     if(argv[0][1]=='-') { //long option
-      for(i=0;i<sizeof(commands)/sizeof(struct sub_command);++i) {
-        if(strcmp(&argv[0][2],commands[i].name)==0) {
-          int result= commands[i].call(argc,argv);
-          if(commands[i].terminating){
+      for(i=0;i<sizeof(options)/sizeof(struct sub_command);++i) {
+        if(strcmp(&argv[0][2],options[i].name)==0) {
+          int result= options[i].call(argc,argv);
+          if(options[i].terminating){
             exit(result);
           }else {
             return result;
@@ -56,10 +63,10 @@ int proccmd(int argc,char** argv) {
       }
     }else { //short option
       if(argv[0][2]=='\0') {
-        for(i=0;i<sizeof(commands)/sizeof(struct sub_command);++i) {
-          if(position_char(&argv[0][1],(char*)commands[i].short_name)!=-1) {
-            int result= commands[i].call(argc,argv);
-            if(commands[i].terminating){
+        for(i=0;i<sizeof(options)/sizeof(struct sub_command);++i) {
+          if(position_char(&argv[0][1],(char*)options[i].short_name)!=-1) {
+            int result= options[i].call(argc,argv);
+            if(options[i].terminating){
               exit(result);
             }else {
               return result;
@@ -72,9 +79,21 @@ int proccmd(int argc,char** argv) {
     char *l,*r;
     l=subseq(argv[0],0,pos);
     r=subseq(argv[0],pos+1,0);
-    set_opt(&local_opt,l,r,0);
+    if(r)
+      set_opt(&local_opt,l,r,0);
+    else{
+      struct opts* opt=global_opt;
+      struct opts** opts=&opt;
+      unset_opt(opts, l);
+      global_opt=*opts;
+    }
     s(l),s(r);
   }else {
+    for(i=0;i<sizeof(commands)/sizeof(struct sub_command);++i) {
+      if(strcmp(&argv[0][0],commands[i].name)==0) {
+        exit(commands[i].call(argc,argv));
+      }
+    }
     printf("invalid command %s\n",argv[0]);
   }
   return 1;
@@ -88,7 +107,7 @@ int main (int argc,char **argv) {
   global_opt=load_opts(path);
   s(path);
   if(argc==1) {
-    char* tmp[]={"--help"};
+    char* tmp[]={" usage"};
     proccmd(0,tmp);
   }else{
     for(i=1;i<argc;i+=proccmd(argc-i,&argv[i]));
