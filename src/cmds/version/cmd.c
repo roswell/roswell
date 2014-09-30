5f88b574719a0812bb180acc887415d30b76c62e
SANO Masatoshi <snmsts@gmail.com>
2014-10-01 06:41:48 +0900

    version option output a bit more information

--- src/cmds/version/cmd.c
+++ src/cmds/version/cmd.c
@@ -1,11 +1,29 @@
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
 #endif
+#ifdef HAVE_LIBCURL
+#  include <curl/curl.h>
+#endif
+#ifdef HAVE_LIBARCHIVE
+#  include <archive.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int cmd_version(int argc,char **argv)
+int cmd_version(int argc,char **argv,struct sub_command* cmd)
 {
-  fprintf(stderr,"%s\n",PACKAGE_STRING);
-  return EXIT_SUCCESS;
+  fprintf(stderr,"%s",PACKAGE_STRING);
+  if(strcmp(argv[0],"--version")!=0) {
+#ifdef HAVE_LIBCURL
+    fprintf(stderr,"\nlibcurl/%s",LIBCURL_VERSION);
+#endif
+#ifdef HAVE_LIBARCHIVE
+    fprintf(stderr,"\n%s",ARCHIVE_VERSION_STRING);
+#endif
+    fprintf(stderr,"\n");
+    return 1;
+  }
+  fprintf(stderr,"\n");
+  return 1;
 }
