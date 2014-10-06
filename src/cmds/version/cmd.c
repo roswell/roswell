e2e4138474ca2395671d546b1a11305752c0d892
SANO Masatoshi <snmsts@gmail.com>
2014-10-06 20:42:20 +0900

    change format of showing libcurl version

--- src/cmds/version/cmd.c
+++ src/cmds/version/cmd.c
@@ -16,7 +16,7 @@ int cmd_version(int argc,char **argv,struct sub_command* cmd)
   fprintf(stderr,"%s",PACKAGE_STRING);
   if(strcmp(argv[0],"--version")!=0) {
 #ifdef HAVE_LIBCURL
-    fprintf(stderr,"\nlibcurl/%s",LIBCURL_VERSION);
+    fprintf(stderr,"\nlibcurl %s",LIBCURL_VERSION);
 #endif
 #ifdef HAVE_LIBARCHIVE
     fprintf(stderr,"\n%s",ARCHIVE_VERSION_STRING);
