b66327585e4e231d31768029526e3e7a5eef440b
SANO Masatoshi <snmsts@gmail.com>
2014-11-29 06:01:39 +0900

    muffle warnings (exported symbols are exists when reload init.lisp)

--- src/lisp/init.lisp
+++ src/lisp/init.lisp
@@ -6,25 +6,26 @@
 
 (defpackage :ros
   (:use :cl)
-  (:shadow :load :eval :package :restart :print :write))
+  (:shadow :load :eval :package :restart :print :write)
+  (:export :run :*argv* :script :quicklisp :getenv))
 
 (in-package :ros)
 (push :ros.init *features*)
 (defvar *verbose* 0)
-(export (defvar *argv* nil))
+(defvar *argv* nil)
 
 ;; small tools
-(export
- (defun getenv (x)
-   #+sbcl(sb-posix:getenv x)
-   #-sbcl(error "not implemented")))
+(defun getenv (x)
+  #+sbcl(sb-posix:getenv x)
+  #-sbcl(error "not implemented"))
 
-(export
- (defun quicklisp ()
-   (unless (find :quicklisp *features*)
-     (let((*read-eval*))
-       (cl:load (second (assoc "quicklisp" (read-from-string (getenv "ROS_OPTS"))
-                               :test 'equal)))))))
+(defun quicklisp ()
+  (unless (find :quicklisp *features*)
+    (cl:load
+     (second (assoc "quicklisp"
+                    (let((*read-eval*))
+                      (read-from-string (getenv "ROS_OPTS")))
+                    :test 'equal)))))
 
 ;;
 
@@ -76,28 +77,28 @@
   (declare (ignorable cmd rest))
   (cl:write (cl:eval (read-from-string arg))))
 
-(export
- (defun script (cmd arg &rest rest)
-   (declare (ignorable cmd arg))
-   (setf *argv* rest)
-   (with-open-file (in arg)
-     (let ((line(read-line in)))
-       (cl:load (make-concatenated-stream
-                 (make-string-input-stream
-                  (format nil "(cl:setf cl:*load-pathname* #P~S)(cl:setf cl:*load-truename* (truename cl:*load-pathname*))~A" arg
-                          (if (equal (subseq line 0 (min (length line) 2)) "#!")
-                              "" line)))
-                 in
-                 (make-string-input-stream
-                  (if (eql cmd :script)
-                      "(cl:apply 'main ros:*argv*)"
-                      ""))))))))
+(defun script (cmd arg &rest rest)
+  (declare (ignorable cmd arg))
+  (setf *argv* rest)
+  (if (probe-file arg)
+      (with-open-file (in arg)
+        (let ((line(read-line in)))
+          (cl:load (make-concatenated-stream
+                    (make-string-input-stream
+                     (format nil "(cl:setf cl:*load-pathname* #P~S)(cl:setf cl:*load-truename* (truename cl:*load-pathname*))~A" arg
+                             (if (equal (subseq line 0 (min (length line) 2)) "#!")
+                                 "" line)))
+                    in
+                    (make-string-input-stream
+                     (if (eql cmd :script)
+                         "(cl:apply 'main ros:*argv*)"
+                         ""))))))
+      (format t "script ~S is not exist~%" arg)))
 
 (defun load (x file)
   (declare (ignore x))
   (cl:load file))
 
-(export 
- (defun run (list)
-   (loop :for elt :in list
-      :do (apply (intern (string (first elt)) (find-package :ros)) elt))))
+(defun run (list)
+  (loop :for elt :in list
+     :do (apply (intern (string (first elt)) (find-package :ros)) elt)))
