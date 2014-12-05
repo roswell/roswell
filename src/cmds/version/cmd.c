64954aa1ba62e9d7b8a6fa4ef973083327b4131a
SANO Masatoshi <snmsts@gmail.com>
2014-12-05 23:05:02 +0900

    generated.h doesn't work well so revert.

--- src/cmds/version/cmd.c
+++ src/cmds/version/cmd.c
@@ -12,16 +12,11 @@
 #include <stdlib.h>
 #include "util.h"
 #include "opt.h"
-#include "generated.h"
 
 int cmd_version(int argc,char **argv,struct sub_command* cmd)
 {
   fprintf(stderr,"%s",PACKAGE_STRING);
-  if(strlen(GIT_COMMIT)>0) {
-    fprintf(stderr," (%s)",GIT_COMMIT);
-  }
   if(strcmp(argv[0],"--version")!=0) {
-    fprintf(stderr,"\n%s",CC_VERSION);
 #ifdef HAVE_LIBCURL
     fprintf(stderr,"\nlibcurl %s",LIBCURL_VERSION);
 #endif
