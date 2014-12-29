dc491e0f7847851c2d3c96ba35c9ef28c35c1cba
SANO Masatoshi <snmsts@gmail.com>
2014-12-29 22:54:05 +0900

    work on msys a bit...

--- src/lisp/install/msys.lisp
+++ src/lisp/install/msys.lisp
@@ -1,39 +1,37 @@
 (in-package :ros.install)
+(ql:quickload :zip)
 
-(defun msys-extract (argv)
-  (format t "extracting msys archive.~%")
-  (expand (merge-pathnames "archives/msys2-base-i686-20141113.tar.xz" (homedir))
-          (merge-pathnames "impls/ho/" (homedir)))
-  (format t "extracted~%")
-  (cons t argv))
+;; experiments... on repl
+;;(setq *ros-path* "C:/MinGW/msys/1.0/local/bin/ros")
+;;(setq *home-path* #p"c:/Users/Vagrant/.roswell/")
 
-(defun msys-and-exit (cmd)
-  (with-open-file (out (ensure-directories-exist
-                        (merge-pathnames "impls/ho/msys32/etc/post-install/10msys.post"
-                                         (homedir)))
-                       :direction :output
-                       :if-exists :supersede
-                       :if-does-not-exist :create)
-    (format out "~A~%exit~%" cmd))
-  (uiop/run-program:run-program
-   (namestring (merge-pathnames "impls/ho/msys32/mingw32_shell.bat" (homedir)))
-   :output t))
 
-(defun msys-setup (argv)
-  (format t "first launch~%")
-  (msys-and-exit "")
-  (format t "first done~%")
-  (format t "second launch~%")
-  (msys-and-exit "pacman -Syu --noconfirm")
-  (format t "second done~%")
-  (format t "third launch~%")
-  (msys-and-exit "pacman -S mingw-w64-i686-gcc --noconfirm")
-  (format t "third done~%")
+;; strategy to obtain toolchain for windows
+;; sbcl-bin -> quicklisp(until here should be finished when this file is loaded)
+;; -> zip:unzip -> 7za.exe -> mingw-get
+;; reference for mingw-get : https://chocolatey.org/packages/mingw-get ?
+
+(defvar *7za-archive* "http://jaist.dl.sourceforge.net/project/sevenzip/7-Zip/9.20/7za920.zip")
+
+(defun unzip (path output-path)
+  (zip:unzip
+   path
+   output-path))
+
+(defun msys-setup-7za (argv)
+  ;;[todo] stop download if file are already exists...
+  (let ((archive (let ((pos (position #\/ *7za-archive* :from-end t)))
+                   (when pos
+                     (merge-pathnames (format nil "archives/~A" (subseq *7za-archive* (1+ pos))) (homedir)))))
+        (prefix (merge-pathnames (format nil "impls/~A/~A/~A/" (uname-m) (uname) "7za") (homedir))))
+    (ros.install::download *7za-archive*
+                           archive)
+    (unzip archive (ensure-directories-exist prefix)))
   (cons t argv))
 
 (setq *install-cmds*
-      (list ;;'msys-extract
-            'msys-setup))
+      (list
+       'msys-setup-7za))
 
 (defun msys-help (argv)
   (format t "hoge~%")
