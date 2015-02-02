157bd0b2e29401942c31a9ba925dfa12966105e5
SANO Masatoshi <snmsts@gmail.com>
2015-02-03 05:44:02 +0900

    now compile option for sbcl can be checked at /share/features.lisp-expr.

--- src/lisp/install/sbcl.lisp
+++ src/lisp/install/sbcl.lisp
@@ -143,6 +143,25 @@
     (format t"done.~%"))
   (cons t argv))
 
+(defun sbcl-backup-features (argv)
+  (let ((src (get-opt "src")) origin opts)
+    (with-open-file (out (merge-pathnames "share/features.lisp-expr" (get-opt "prefix"))
+                         :direction :output
+                         :if-exists :supersede
+                         :if-does-not-exist :create)
+      (flet ((read-from-file (f)
+               (with-open-file (in (merge-pathnames f src))
+                 (read in))))
+        (setq origin (funcall (compile nil (read-from-file "local-target-features.lisp-expr"))
+                              (read-from-file "base-target-features.lisp-expr"))
+              opts (funcall (if (probe-file (merge-pathnames #1="customize-target-features.lisp" src))
+                                (compile nil (read-from-file #1#))
+                                #'identity) (copy-list origin)))
+        (format out "(:+ ~s)~%(:- ~s)~%"
+                (set-difference opts origin)
+                (set-difference origin opts)))))
+  (cons t argv))
+
 (defun sbcl-clean (argv)
   (format t "~&Cleaning~%")
   (let ((src (get-opt "src")))
@@ -185,6 +204,7 @@
             'sbcl-config
             'sbcl-make
             'sbcl-install
+            'sbcl-backup-features
             'sbcl-clean
             'setup))
 
