6735c83444b6d674710d4865653c2f206156218e
SANO Masatoshi <snmsts@gmail.com>
2015-02-05 00:39:02 +0900

    muffle message on setup

--- src/cmd-setup.c
+++ src/cmd-setup.c
@@ -7,22 +7,26 @@ extern char** argv_orig;
 int cmd_setup(int argc, const char **argv)
 {
   char* v="";
+  int ret;
   if(verbose==1)
     v="-v ";
   if(verbose==2)
     v="-v -v ";
+  char* sbcl_bin_version=get_opt("sbcl-bin.version",0);
   char* sys=cat(argv_orig[0]," ",v,"install sbcl-bin",NULL);
   fprintf(stderr,"setting up sbcl-bin\n");
-  system(sys);
+  if(!sbcl_bin_version)
+    ret=system(sys);
+  else fprintf(stderr,"already have sbcl-bin\n");
   s(sys);
   sys=cat(argv_orig[0]," ",v,"install quicklisp",NULL);
   fprintf(stderr,"setting up quicklisp\n");
-  system(sys);
+  ret=system(sys);
   s(sys);
 #ifdef _WIN32
   sys=cat(argv_orig[0]," ",v,"install 7zip",NULL);
   fprintf(stderr,"setting up 7zip\n");
-  system(sys);
+  ret=system(sys);
   s(sys);
 #endif
   return 0;
