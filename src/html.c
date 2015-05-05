9c08457b45d12f3b9526046c3809147efbca9628
SANO Masatoshi <snmsts@gmail.com>
2015-05-05 14:22:31 +0900

    html.c reduce lines

--- src/html.c
+++ src/html.c
@@ -1,9 +1,4 @@
-#include "util.h"
-
-struct attr {
-  char* name;
-  char* value;
-};
+#include "opt.h"
 
 struct tag {
   int type; /* 0 for not tag
@@ -14,7 +9,7 @@ struct tag {
 };
 
 struct Cons* attralloc(void) {
-  struct attr* ret=alloc(sizeof(struct attr));
+  struct opts* ret=alloc(sizeof(struct opts));
   ret->name=NULL;
   ret->value=NULL;
   return toPointer(cons(ret,(LVal)NULL));
@@ -23,8 +18,8 @@ struct Cons* attralloc(void) {
 void attrsfree(struct Cons* a) {
   struct Cons* next;
   for(;a;) {
-    struct attr* p=(struct attr*)firstp((LVal)a);
-    s(p->name),s(p->value);
+    struct opts* p=(struct opts*)firstp((LVal)a);
+    s((char*)p->name),s((char*)p->value);
     next=(struct Cons*)a->next;
     dealloc(a);
     a=next;
@@ -32,79 +27,62 @@ void attrsfree(struct Cons* a) {
 }
 
 LVal parse_attr(char* str) {
-  int i;
+  int i,j;
   int pos;
   char* name;
   char* val;
   struct Cons tmp;
   struct Cons *ret=&tmp,*cur=ret;
+  struct opts* p;
   tmp.next=(LVal)NULL;
   for(i=0;str[i]!='\0';++i) {
     name=NULL;
     val=NULL;
     /*skip space*/
     pos=position_char_not(" \t\r\n",&str[i]);
-    if(pos!=-1){
+    if(pos!=-1) {
       i+=pos;
-    }else{
-      continue;
-    }
+    }else continue;
     /*attr name */
     pos=position_char("\"'>/= \t\r\n",&str[i]);
-    if(pos!=-1){
-      name=subseq(&str[i],0,pos);
-      i+=pos;
-    }
+    if(pos!=-1)
+      name=subseq(&str[i],0,pos),i+=pos;
     /*skip space*/
     pos=position_char_not(" \t\r\n",&str[i]);
-    if(pos!=-1){
+    if(pos!=-1)
       i+=pos;
-    }
-    if(name) {
-      struct attr* p;
-      if(str[i]=='=') {
-	int j=0;
-	/*skip space*/
-	pos=position_char_not(" \t\r\n",&str[++i]);
-	if(pos!=-1){
-	  i+=pos;
-	}
-	if(pos!=-1) {
-	  i+=pos;
-	  switch(str[i]) {
-	  case '\'':
-	    j=position_char("'",&str[i+1]);
-	    if(j!=-1) {
-	      val=subseq(&str[i+1],0,j);
-	      i+=j+1;
-	    }
-	    break;
-	  case '"':
-	    for(j=1;str[i+j]!='"';++j) {
-	      if(str[i+j]=='\\')
-		++j;
-	    }
-	    val=subseq(&str[i],1,j);
-	    i+=j;
-	    break;
-	  default:
-	    pos=position_char(" \t\r\n",&str[i]);
-	    val=subseq(&str[i],0,pos);
-	    break;
-	  }
-	  cur->next=(LVal)attralloc();;
-	  p=(struct attr*)firstp((LVal)cur->next);
-	  p->name=name;
-	  p->value=val;
-	  cur=(struct Cons *)cur->next;
-	}else {
-	  cur->next=(LVal)attralloc();
-	  p=(struct attr*)firstp((LVal)cur->next);
-	  p->name=name;
-	  cur=(struct Cons *)cur->next;
-	}
+    if(!(name && str[i]=='='))
+      continue;
+    cur->next=(LVal)attralloc();;
+    p=(struct opts*)firstp((LVal)cur->next);
+    p->name=name;
+    /*skip space*/
+    pos=position_char_not(" \t\r\n",&str[++i]);
+    if(pos!=-1) {
+      i+=pos;
+      switch(str[i]) {
+      case '\'':
+        j=position_char("'",&str[i+1]);
+        if(j!=-1) {
+          val=subseq(&str[i+1],0,j);
+          i+=j+1;
+        }
+        break;
+      case '"':
+        for(j=1;str[i+j]!='"';++j)
+          if(str[i+j]=='\\')
+            ++j;
+        val=subseq(&str[i],1,j);
+        i+=j;
+        break;
+      default:
+        pos=position_char(" \t\r\n",&str[i]);
+        val=subseq(&str[i],0,pos);
+        break;
       }
+      p->value=val;
     }
+    cur=(struct Cons *)cur->next;
   }
   return (LVal)ret->next;
 }
@@ -147,9 +125,8 @@ LVal delete_not_open_tags(LVal t) {
       next=Next(Next(tag));
       tagfree(Next(tag));
       ((struct Cons*)tag)->next=next;
-    }else{
+    }else
       tag=Next(tag);
-    }
   }
   return ret;
 }
@@ -165,9 +142,8 @@ LVal delete_not_tags(char* tags,LVal tag) {
       next=Next(Next(tag));
       tagfree(Next(tag));
       ((struct Cons*)tag)->next=next;
-    }else {
+    }else
       tag=Next(tag);
-    }
   }
   return (LVal)ret;
 }
@@ -180,15 +156,14 @@ LVal filter_href(LVal t) {
     href=NULL;
     LVal a=(LVal)((struct tag*)firstp(tags))->attr;
     for(;a;a=Next(a)) {
-      struct attr* v=(struct attr*)firstp(a);
+      struct opts* v=(struct opts*)firstp(a);
       if(strcmp(v->name,"href")==0) {
 	href=q(v->value);
 	break;
       }
     }
-    if(href) {
+    if(href)
       ret=(LVal)toPointer(conss(href,ret));
-    }
   }
   return ret;
 }
@@ -196,104 +171,103 @@ LVal filter_href(LVal t) {
 LVal parse_tags(FILE* fp,LVal before,int mode) {
   LVal current=tagalloc();
   char str[2]={'\0','\0'};
-  int c;
+  int c,i=0,token_count=0;
   char* buf=q("");
-  if(mode==0) { /* wait for '<' */
+  switch(mode) {
+  case 0: /* wait for '<' */
     ((struct tag*)firstp(current))->type=0;
     while((c=fgetc(fp))!=EOF) {
       if(c=='<') {
-	if(strlen(buf)==0) {
-	  tagfree(current);
-	  s(buf);
-	  return parse_tags(fp,before,1);
-	}else {
-	  ((struct Cons*)current)->next=parse_tags(fp,current,1);
-	  s(buf);
-	  return current;
-	}
-      }else {
-	str[0]=c;
-	buf=s_cat2(buf,q(str));
-      }
+        if(strlen(buf)==0) {
+          tagfree(current);
+          s(buf);
+          return parse_tags(fp,before,1);
+        }else {
+          ((struct Cons*)current)->next=parse_tags(fp,current,1);
+          s(buf);
+          return current;
+        }
+      }else
+        str[0]=c,buf=s_cat2(buf,q(str));
     }
-  }else if (mode==1) { /* wait for '>' */
-    int i=0;
-    int token_count=0;
+    break;
+  case 1: /* wait for '>' */
     ((struct tag*)firstp(current))->type=0;
-    
     while((c=fgetc(fp))!=EOF) {
       if(i==0) {
-	if(c=='/')
-	  ((struct tag*)firstp(current))->type=2;
-	else {
-	  ((struct tag*)firstp(current))->type=1;
-	  str[0]=c;
-	  buf=s_cat2(buf,q(str));
-	}
-	++i;
-	continue;
+        if(c=='/')
+          ((struct tag*)firstp(current))->type=2;
+        else {
+          ((struct tag*)firstp(current))->type=1;
+          str[0]=c;
+          buf=s_cat2(buf,q(str));
+        }
+        ++i;
+        continue;
       }
       if(c=='>') {
-	char *buf2;
-	if(((struct tag*)firstp(current))->type==2) {
-	  int pos=position_char(" \t\r\n",buf);
-	  if(pos!=-1) {
-	    buf2=subseq(buf,0,pos);
-	    s(buf);
-	    buf=buf2;
-	    ((struct tag*)firstp(current))->name=q(buf);
-	  }else {
-	    ((struct tag*)firstp(current))->name=buf;
-	    buf=q("");
-	  }
-	}else if(((struct tag*)firstp(current))->type==1) {
-	  int pos=position_char(" \t\r\n",buf);
-	  if(pos!=-1) {
-	    ((struct tag*)firstp(current))->name=subseq(buf,0,pos);
-	    buf2=subseq(buf,pos,0);
-	    ((struct tag*)firstp(current))->attr=(struct Cons*)parse_attr(buf2);
-	    s(buf);
-	    buf=buf2;
-	  }else {
-	    ((struct tag*)firstp(current))->name=buf;
-	    buf=q("");
-	  }
-	}
-	if(strcmp(((struct tag*)firstp(current))->name,"script")==0) {
-	  ((struct Cons*)current)->next=parse_tags(fp,current,2);
-	}else{
-	  ((struct Cons*)current)->next=parse_tags(fp,current,0);
-	}
-	s(buf);
-	return current;
+        char *buf2;
+        if(((struct tag*)firstp(current))->type==2) {
+          int pos=position_char(" \t\r\n",buf);
+          if(pos!=-1) {
+            buf2=subseq(buf,0,pos);
+            s(buf);
+            buf=buf2;
+            ((struct tag*)firstp(current))->name=q(buf);
+          }else {
+            ((struct tag*)firstp(current))->name=buf;
+            buf=q("");
+          }
+        }else if(((struct tag*)firstp(current))->type==1) {
+          int pos=position_char(" \t\r\n",buf);
+          if(pos!=-1) {
+            ((struct tag*)firstp(current))->name=subseq(buf,0,pos);
+            buf2=subseq(buf,pos,0);
+            ((struct tag*)firstp(current))->attr=(struct Cons*)parse_attr(buf2);
+            s(buf);
+            buf=buf2;
+          }else {
+            ((struct tag*)firstp(current))->name=buf;
+            buf=q("");
+          }
+        }
+        if(strcmp(((struct tag*)firstp(current))->name,"script")==0) {
+          ((struct Cons*)current)->next=parse_tags(fp,current,2);
+        }else{
+          ((struct Cons*)current)->next=parse_tags(fp,current,0);
+        }
+        s(buf);
+        return current;
       }else {
-	str[0]=c;
-	buf=s_cat2(buf,q(str));
+        str[0]=c;
+        buf=s_cat2(buf,q(str));
       }
       ++i;
     }
-  }else if (mode==2) { /* wait for '</' */
+    break;
+  case 2: /* wait for '</' */
     ((struct tag*)firstp(current))->type=0;
     while((c=fgetc(fp))!=EOF) {
       if(c=='<') {
-	if((c=fgetc(fp))!=EOF && c=='/') {
-	  ungetc('/',fp);
-	  if(strlen(buf)==0) {
-	    tagfree(current);
-	    s(buf);
-	    return parse_tags(fp,current,1);
-	  }else {
-	    ((struct Cons*)current)->next=parse_tags(fp,current,1);
-	    s(buf);
-	    return current;
-	  }
-	}
-	ungetc('/',fp);
+        if((c=fgetc(fp))!=EOF && c=='/') {
+          ungetc('/',fp);
+          if(strlen(buf)==0) {
+            tagfree(current);
+            s(buf);
+            return parse_tags(fp,current,1);
+          }else {
+            ((struct Cons*)current)->next=parse_tags(fp,current,1);
+            s(buf);
+            return current;
+          }
+        }
+        ungetc('/',fp);
       }else {
-	str[0]=c;
-	buf=s_cat2(buf,q(str));
+        str[0]=c;
+        buf=s_cat2(buf,q(str));
       }
     }
+    break;
   }
   s(buf);
   return current;
@@ -317,7 +291,6 @@ LVal atag_list(char* filename) {
 
 LVal filter_sbcl_uri(LVal v) {
   char* str=subseq(firsts(v),-3,0);
-
   if(strcmp(str,"bz2")==0 ||
      strcmp(str,"msi")==0) {
     char* u=uname();
@@ -353,12 +326,8 @@ LVal filter_sbcl_uri(LVal v) {
 char* sbcl_bin(char* file) {
   char* str;
   LVal ret3,ret2,ret;
-  if(verbose>0) {
-    char* u=uname();
-    char* m=uname_m();
-    fprintf(stderr,"uname=%s uname-m=%s\n",u,m);
-  }
-
+  if(verbose>0)
+    fprintf(stderr,"uname=%s uname-m=%s\n",uname(),uname_m());
   ret=atag_list(file);
   ret2=remove_if_not1(filter_sbcl_uri,ret);
   if(ret2==(LVal)NULL) {
