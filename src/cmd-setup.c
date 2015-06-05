c9ff6013dbd2039b1a15a2d7e14073ae72537393
SANO Masatoshi <snmsts@gmail.com>
2015-06-06 01:30:11 +0900

    speed up subcmd by using dumped version

--- src/cmd-setup.c
+++ src/cmd-setup.c
@@ -1,33 +1,26 @@
 #include "opt.h"
 
 extern char** argv_orig;
+
+#define CMD_SETUP_SYSTEM(sys,msg) { \
+    fprintf(stderr,"%s",msg);       \
+    ret=system(sys);                \
+    s(sys);                         \
+    if(ret) return ret;             \
+  }
+
 int cmd_setup(int argc, const char **argv) {
-  char* v="";
+  char* v=verbose==1?"-v ":(verbose==2?"-v -v ":"");
   int ret=1;
-  if(verbose==1)
-    v="-v ";
-  if(verbose==2)
-    v="-v -v ";
   char* sbcl_bin_version=get_opt("sbcl-bin.version",0);
-  char* sys=cat(argv_orig[0]," ",v,"install sbcl-bin",NULL);
-  fprintf(stderr,"setting up sbcl-bin\n");
-  if(!sbcl_bin_version)
-    ret=system(sys);
-  else fprintf(stderr,"already have sbcl-bin\n"),ret=0;
-  s(sys);
-  if(ret)
-    return ret;
-  sys=cat(argv_orig[0]," ",v,"install quicklisp",NULL);
-  fprintf(stderr,"setting up quicklisp\n");
-  ret=system(sys);
-  s(sys);
-  if(ret)
-    return ret;
+  if(!sbcl_bin_version) {
+    CMD_SETUP_SYSTEM(cat(argv_orig[0]," ",v,"install sbcl-bin",NULL),"setting up sbcl-bin\n");
+  }else
+    fprintf(stderr,"already have sbcl-bin\n");
+  CMD_SETUP_SYSTEM(cat(argv_orig[0]," ",v,"install quicklisp",NULL),"setting up quicklisp\n");
+  CMD_SETUP_SYSTEM(cat(argv_orig[0]," ",v,"roswell-internal-core-build",NULL),"makeing core for roswell\n");
 #ifdef HAVE_WINDOWS_H
-  sys=cat(argv_orig[0]," ",v,"install 7zip",NULL);
-  fprintf(stderr,"setting up 7zip\n");
-  ret=system(sys);
-  s(sys);
+  CMD_SETUP_SYSTEM(cat(argv_orig[0]," ",v,"install 7zip",NULL),"setting up 7zip\n");
 #endif
   return ret;
 }
