roswell/roswell

commit 50bd14adecc011638a3254b465cf8afe3b4ab63d

Raise an error when a file trying to execute is not found.

src/lisp/init.lisp
@@ -29,6 +29,8 @@
 
   (defun execvp (program args)
     "Replace current executable with another one."
+    (unless (probe-file program)
+      (error "Cannot exec ~S: No such file or directory" program))
     (let ((a-args (sb-alien:make-alien sb-alien:c-string
                                        (+ 1 (length args)))))
       (unwind-protect

commit 2c5c07a51d7babd0cba8e3fae204c62d7ee3065b

Fix execvp not to raise a 'No such file' error if the given 'program' is located in $PATH like 'ros'.
It's been broken since 50bd14adecc011638a3254b465cf8afe3b4ab63d.

src/lisp/init.lisp
@@ -29,8 +29,6 @@
 
   (defun execvp (program args)
     "Replace current executable with another one."
-    (unless (probe-file program)
-      (error "Cannot exec ~S: No such file or directory" program))
     (let ((a-args (sb-alien:make-alien sb-alien:c-string
                                        (+ 1 (length args)))))
       (unwind-protect
@@ -41,8 +39,11 @@
                          item))
              (when (minusp
                     (%execvp program a-args))
-               (error "execvp(3) failed. (Code=~D)"
-                      (sb-impl::get-errno))))
+               (let ((errno (sb-impl::get-errno)))
+                 (case errno
+                   (2 (error "No such file or directory: ~S" program))
+                   (otherwise
+                    (error "execvp(3) failed. (Code=~D)" errno))))))
         (sb-alien:free-alien a-args)))))
 
 (defun setenv (name value)