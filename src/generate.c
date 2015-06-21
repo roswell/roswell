5dc5478bd7831ba36f600c4d07e7f1a9bcbefaa6
SANO Masatoshi <snmsts@gmail.com>
2015-06-21 22:06:41 +0900

    changes to support cross compiling

--- src/generate.c
+++ src/generate.c
@@ -1,32 +1,36 @@
 #include "util.h"
 int verbose=0;
-char** argv_orig;
 char* lispdir(){return q("");}
+
 int main(int argc,char **argv) {
-  char* cmd=cat(argv[1]," --version",NULL);
-  char* result=system_(cmd);
-  int pos=position_char("\r\n",result);
-  result[pos]='\0';
-  printf("#define ROS_COMPILE_ENVIRONMENT \"%s on ",result);
-  printf("%s",uname());
-  printf("(%s)\"\n",uname_m());
-  result=which("git"EXE_EXTENTION);
-  if (file_exist_p(result) && directory_exist_p("../.git/")) {
-    result=system_("git log -n 1 --oneline");
-    pos=position_char(" ",result);
-    result[pos]='\0';
-    printf("#define ROS_REVISION \"commit:%s\"\n",result);
-  }else {
-    printf("#define ROS_REVISION \"\"\n");
-  }
-  cmd=cat("sh -c 'cd ",LISP_PATH,"; pwd -W 2>/dev/null'",NULL);
-  result=system_(cmd);
-  if(result[0]!='\0') {
+  char buf[1000];
+  int pos;
+  char *result=fgets(buf,1000,stdin);
+  switch(argv[1][0]) {
+  case '1':
     pos=position_char("\r\n",result);
     result[pos]='\0';
-    substitute_char('\\','/',result);
-    result=escape_string(result);
-    printf("#define WIN_LISP_PATH \"%s\\\\\"\n",result);
+    printf("#define ROS_COMPILE_ENVIRONMENT \"%s on ",buf);
+    printf("%s",uname());
+    printf("(%s)\"\n",uname_m());
+    break;
+  case '2':
+    if(strlen(result)!=0) {
+      pos=position_char(" ",result);
+      result[pos]='\0';
+      printf("#define ROS_REVISION \"commit:%s\"\n",result);
+    }else
+      printf("#define ROS_REVISION \"\"\n");
+    break;
+  case '3':
+    if(result[0]!='\0'&&result[0]!='\r'&&result[0]!='\n') {
+      pos=position_char("\r\n",result);
+      result[pos]='\0';
+      substitute_char('\\','/',result);
+      result=escape_string(result);
+      printf("#define WIN_LISP_PATH \"%s\\\\\"\n",result);
+    }
+    break;
   }
   exit(EXIT_SUCCESS);
 }
