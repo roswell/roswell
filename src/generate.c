580b75b9961bad3054fd7a0981e91f129227f334
SANO Masatoshi <snmsts@gmail.com>
2015-06-21 23:30:29 +0900

    fix error when compile without git environment.

--- src/generate.c
+++ src/generate.c
@@ -8,14 +8,16 @@ int main(int argc,char **argv) {
   char *result=fgets(buf,1000,stdin);
   switch(argv[1][0]) {
   case '1':
-    pos=position_char("\r\n",result);
-    result[pos]='\0';
-    printf("#define ROS_COMPILE_ENVIRONMENT \"%s on ",buf);
-    printf("%s",uname());
-    printf("(%s)\"\n",uname_m());
+    if(result) {
+      pos=position_char("\r\n",result);
+      result[pos]='\0';
+      printf("#define ROS_COMPILE_ENVIRONMENT \"%s on ",buf);
+      printf("%s",uname());
+      printf("(%s)\"\n",uname_m());
+    }
     break;
   case '2':
-    if(strlen(result)!=0) {
+    if(result && strlen(result)!=0) {
       pos=position_char(" ",result);
       result[pos]='\0';
       printf("#define ROS_REVISION \"commit:%s\"\n",result);
@@ -23,7 +25,7 @@ int main(int argc,char **argv) {
       printf("#define ROS_REVISION \"\"\n");
     break;
   case '3':
-    if(result[0]!='\0'&&result[0]!='\r'&&result[0]!='\n') {
+    if(result && result[0]!='\0'&&result[0]!='\r'&&result[0]!='\n') {
       pos=position_char("\r\n",result);
       result[pos]='\0';
       substitute_char('\\','/',result);
