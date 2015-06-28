7ee2aff872dfe3961e63689950a40e461cc213f3
SANO Masatoshi <snmsts@gmail.com>
2015-06-28 18:19:09 +0900

    re-imp a bit...

--- lsp.go
+++ lsp.go
@@ -5,11 +5,19 @@ import (
 	"os"
 )
 
+var topOptions []subCommand
+
 func Proccmd(argv []string, option []string, command []string) int {
 	return 1
 }
 
 func main() {
+  /*options*/
+  /* toplevel */
+  topOptions=AddCommand(topOptions,"wrap","-w",0/*dummy*/,1,0,"shell wrapper CODE to run in roswell","CODE");
+  topOptions=AddCommand(topOptions,"image","-m",0/*dummy*/,1,0,"build from Lisp image IMAGE","IMAGE");
+  topOptions=AddCommand(topOptions,"lisp","-L",0/*dummy*/,1,0,"try use these LISP implementation","NAME");
+
 	if len(os.Args) == 1 {
 		fmt.Print("help")
 	} else {
--- opt.go
+++ opt.go
@@ -3,15 +3,16 @@ package main
 type subCommand struct {
 	name        string
 	shortName   string
-	call        int
+	call        int /*dummy untill learn howto use function in go*/
 	showOpt     int
 	terminating int
 	description string
 	argExample  string
 }
 
-func AddCommand(base []subCommand, name string, shortName string, fnc int,
+func AddCommand(base []subCommand, name string, shortName string, call int,
 	showOpt int, terminating int, description string, argExample string) []subCommand {
 
-	return []subCommand{}
+	return append(base, subCommand{name: name, shortName: shortName, call: call, showOpt: showOpt,
+		terminating: terminating, description: description, argExample: argExample})
 }
