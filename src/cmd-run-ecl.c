f6271b0838a944f392ed28577089c56fbd4a7341
SANO Masatoshi <snmsts@gmail.com>
2015-08-03 01:56:33 +0900

    start implementing ecl some...

---
+++ src/cmd-run-ecl.c
@@ -0,0 +1,41 @@
+#include "opt.h"
+
+char** cmd_run_ecl(int argc,char** argv,struct sub_command* cmd) {
+  char** arg=NULL;
+  char* home=configdir();
+  char* arch=uname_m();
+  char* os=uname();
+  char* impl=(char*)cmd->name;
+  char* version=(char*)cmd->short_name;
+  int offset=6; /*[binpath for ecl] -norc -eval init.lisp
+                   [terminating NULL] that total 5 are default. */
+  int i;
+  char* impl_path= cat(home,"impls",SLASH,arch,SLASH,os,SLASH,impl,SLASH,version,NULL);
+  char* help=get_opt("help",0);
+  char* script=get_opt("script",0);
+  char* image=get_opt("image",0);
+  char* program=get_opt("program",0);
+  char* ecl_version=get_opt("version",0);
+  int paramc=0;
+  char *bin;
+  int issystem=(strcmp("system",version)==0);
+  if(issystem){
+    bin=truename(which("ecl"));
+  }else {
+    bin=cat(impl_path,SLASH,"bin",SLASH,"ecl",EXE_EXTENTION,NULL);
+  }
+  s(arch),s(os);
+  if(help) {
+    offset++;
+  }
+  if(sbcl_version)
+    offset+=1;
+  if(quicklisp)
+    offset+=2;
+  if(program||script)
+    offset+=2;
+  arg=alloc(sizeof(char*)*(offset+argc));
+  arg[paramc++]=q("wrapper-dummy");
+  arg[paramc++]=bin;
+  /* runtime options from here */
+}
