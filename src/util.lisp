be536bcc3fc6bf38905879044917252b0d78dbb8
Masataro Asai <guicho2.71828@gmail.com>
2016-01-16 21:49:41 +0900

    fixed util.lisp:use

--- src/util.lisp
+++ src/util.lisp
@@ -27,21 +27,35 @@
   (ros:roswell (list "config" b a) :string t)
   a)
 
+(defun parse-version-spec (string)
+  "Parse the given version specification string and returns a list of strings (LISP VERSION).
+If it does not contain a version substring, VERSION becomes a null.
+If it is a version string only (detected when it starts from digit-char), LISP becomes NIL.
+Examples:
+ccl-bin/1.11 -> (\"ccl-bin\" \"1.11\")
+ccl-bin      -> (\"ccl-bin\" nil)
+1.11         -> (nil \"1.11\")
+"
+  (let ((pos (position #\/ string)))
+    (if pos
+        (list (subseq string 0 pos) (subseq string (1+ pos)))
+        (if (digit-char-p (aref string 0))
+            (list nil string)
+            (list string nil)))))
+
 (defun use (arg)
+  "Parse the lisp version string (such as ccl-bin/1.11) and set it to the correct config slot(s)"
   (when (and arg
              (ignore-errors
                (ros:roswell `("-L" ,arg "version=t" "run"))))
-    (let* ((pos (position #\/ arg))
-           (lisp (if pos
-                     (subseq arg 0 pos)
-                     arg)))
-      (if pos
-          (progn
-            (config "default.lisp" lisp)
-            (config (format nil "~A.version" lisp)
-                    (subseq arg (1+ pos))))
-          (config (if (digit-char-p (aref lisp 0))
-                      (format nil "~A.version" (config "default.lisp"))
-                      "default.lisp")
-                  lisp)))
+    (destructuring-bind (lisp version) (parse-version-spec arg)
+      (cond ((and lisp version)
+             (setf (config "default.lisp") lisp
+                   (config (format nil "~A.version" lisp)) version))
+            (lisp
+             (setf (config "default.lisp")
+                   lisp))
+            (version
+             (setf (config (format nil "~A.version" (config "default.lisp")))
+                   version))))
     t))
