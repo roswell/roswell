ab319196953fdbfa425705e1f1ba188a15ced779
SANO Masatoshi <snmsts@gmail.com>
2016-02-18 02:53:29 +0900

    support invoke intel 64bit/32bit ccl from 32bit/64 OS. 'ros ccl.bit=32 -L ccl-bin run' 'ros ccl.bit=64 -L ccl-bin run'. to fix 'ros config set ccl.bit 32'.

--- src/cmd-run-ccl.c
+++ src/cmd-run-ccl.c
@@ -1,7 +1,7 @@
 /* -*- tab-width : 2 -*- */
 #include "opt.h"
 
-char* ccl_binname(void) {
+char* ccl_binname(char* bit) {
   char* ret=q("");
   char* _uname_m=uname_m();
   char* _uname=uname();
@@ -19,13 +19,17 @@ char* ccl_binname(void) {
   }else if(strcmp(_uname_m,"armhf")==0)
     ret=s_cat(ret,q("arm"),NULL);
   ret=s_cat(ret,q("cl"),NULL);
-  if(strcmp(_uname_m,"x86-64")==0)
-    ret=s_cat(ret,q("64"),NULL);
+  if((strcmp(_uname_m,"x86-64")==0 &&
+      strcmp(bit,"32")!=0) ||
+     (strcmp(_uname_m,"x86")==0 &&
+      strcmp(bit,"64")==0))
+     ret=s_cat(ret,q("64"),NULL);
   return ret;
 }
 
 char** cmd_run_ccl(int argc,char** argv,struct sub_command* cmd) {
   char** arg=NULL;
+  char* binname=get_opt("ccl.bit",0);
   char* home=configdir();
   char* arch=uname_m();
   char* os=uname();
@@ -42,9 +46,11 @@ char** cmd_run_ccl(int argc,char** argv,struct sub_command* cmd) {
   char* image=get_opt("image",0);
   char* program=get_opt("program",0);
   int issystem=(strcmp("system",version)==0);
+  binname = ccl_binname(binname ?binname:"");
+
   unsetenv("CCL_DEFAULT_DIRECTORY");
   bin=issystem?truename(which(strcmp(impl,"ccl32")==0?"ccl32":"ccl")):
-    s_cat(q(impl_path),q(SLASH),ccl_binname(),q(EXE_EXTENTION),NULL);
+    cat(impl_path,SLASH,binname,EXE_EXTENTION,NULL);
 
   if(ccl_version)
     offset+=1;
@@ -65,14 +71,11 @@ char** cmd_run_ccl(int argc,char** argv,struct sub_command* cmd) {
   if(image||!issystem)
     arg[paramc++]=q("--image-name");
   if(!image) {
-    if(!issystem) {
-      char* binname=ccl_binname();
+    if(!issystem)
       arg[paramc++]=cat(impl_path,SLASH,binname,".image",NULL);
-      s(binname);
-    }
-  }else {
-    arg[paramc++]=cat(impl_path,SLASH,"dump",SLASH,image,".core",NULL);
-  }
+  }else 
+    arg[paramc++]=cat(impl_path,SLASH,"dump",SLASH,image,".",binname,NULL);
+
   arg[paramc++]=q("--eval");
   arg[paramc++]=s_cat(q("(progn #-ros.init(cl:load \""),lispdir(),q("init.lisp"),q("\"))"),NULL);
 
@@ -88,11 +91,10 @@ char** cmd_run_ccl(int argc,char** argv,struct sub_command* cmd) {
     arg[paramc++]=tmp;
   }
 
-  for(i=1;i<argc;++i) {
+  for(i=1;i<argc;++i)
     arg[paramc++]=argv[i];
-  }
 
-  s(impl_path);
+  s(impl_path),s(binname);
 
   arg[paramc]=NULL;
   return arg;
