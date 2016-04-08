b0d8c74fdac23c12c338a4c900d7da8d972fdb1a
SANO Masatoshi <snmsts@gmail.com>
2016-04-09 06:22:18 +0900

    set sbcl-source-location automatically if source are installed.

--- lisp/init.lisp
+++ lisp/init.lisp
@@ -265,6 +265,12 @@ have the latest asdf, and this file has a workaround for this.
            (set symbol (merge-pathnames (format nil "~A/" impl) (symbol-value symbol))))
           (t (cl:print "tbd.....")))))
 
+#+sbcl
+(when (ignore-errors (string-equal (ros:opt "impl") "sbcl-bin" :end1 8))
+  (let ((path (probe-file (merge-pathnames (format nil "src/sbcl-~A/" (lisp-implementation-version)) (opt "homedir")))))
+    (when (probe-file path)
+      (sb-ext:set-sbcl-source-location path))))
+
 (defun source-registry (cmd arg &rest rest)
   (declare (ignorable cmd rest))
   (let ((dir (format nil "~{~A~^:~}"
