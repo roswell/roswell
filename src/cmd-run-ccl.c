a68159c81e4d431d01f99b651f344e8c3dab2a3e
SANO Masatoshi <snmsts@gmail.com>
2016-04-09 12:40:08 +0900

    shorten cmd-run-ccl.c

--- src/cmd-run-ccl.c
+++ src/cmd-run-ccl.c
@@ -28,73 +28,59 @@ char* ccl_binname(char* bit) {
 }
 
 char** cmd_run_ccl(int argc,char** argv,struct sub_command* cmd) {
-  char** arg=NULL;
   char* binname=get_opt("ccl.bit",0);
   char* home=configdir();
   char* arch=uname_m();
   char* os=uname();
   char* impl=(char*)cmd->name;
   char* version=(char*)cmd->short_name;
-  int offset=8;  /*[binpath for ccl] --no-init --quiet --batch --image-name param --eval init.lisp
-                   [terminating NULL] that total 9 are default. */
+  /*[binpath for ccl] --no-init --quiet --batch --image-name param --eval init.lisp
+    [terminating NULL] that total 9 are default. */
   int i;
   char* ccl_version=get_opt("version",0);
-  int paramc=0;
   char* impl_path= cat(home,"impls",SLASH,arch,SLASH,os,SLASH,impl,SLASH,version,NULL);
-  char* bin;
   char* script=get_opt("script",0);
   char* image=get_opt("image",0);
   char* program=get_opt("program",0);
   int issystem=(strcmp("system",version)==0);
+  LVal ret=0;
   binname = ccl_binname(binname?binname:"");
 
   unsetenv("CCL_DEFAULT_DIRECTORY");
-  bin=issystem?truename(which(strcmp(impl,"ccl32")==0?"ccl32":"ccl")):
-    cat(impl_path,SLASH,binname,EXE_EXTENTION,NULL);
 
+  ret=conss(issystem?truename(which(strcmp(impl,"ccl32")==0?"ccl32":"ccl")):
+            cat(impl_path,SLASH,binname,EXE_EXTENTION,NULL),ret);
   if(ccl_version)
-    offset+=1;
-  if(script)
-    offset+=1;
-  if(quicklisp)
-    offset+=2;
-  if(program||script)
-    offset+=2;
-
-  arg=alloc(sizeof(char*)*(offset+argc));
-  arg[paramc++]=bin;
-  if(ccl_version)
-    arg[paramc++]=q("--version");
-  arg[paramc++]=q("--no-init");
-  arg[paramc++]=q("--quiet");
+    ret=conss(q("--version"),ret);
+  ret=conss(q("--no-init"),ret);
+  ret=conss(q("--quiet"),ret);
   if(image||!issystem)
-    arg[paramc++]=q("--image-name");
+    ret=conss(q("--image-name"),ret);
   if(!image) {
     if(!issystem)
-      arg[paramc++]=cat(impl_path,SLASH,binname,".image",NULL);
+      ret=conss(cat(impl_path,SLASH,binname,".image",NULL),ret);
   }else 
-    arg[paramc++]=cat(impl_path,SLASH,"dump",SLASH,image,".",binname,NULL);
+    ret=conss(cat(impl_path,SLASH,"dump",SLASH,image,".",binname,NULL),ret);
 
-  arg[paramc++]=q("--eval");
-  arg[paramc++]=s_cat(q("(progn #-ros.init(cl:load \""),lispdir(),q("init.lisp"),q("\"))"),NULL);
+  ret=conss(q("--eval"),ret);
+  ret=conss(s_cat(q("(progn #-ros.init(cl:load \""),lispdir(),q("init.lisp"),q("\"))"),NULL),ret);
 
   if(quicklisp) {
-    arg[paramc++]=q("--eval");
-    arg[paramc++]=q("(ros:quicklisp)");;
+    ret=conss(q("--eval"),ret);
+    ret=conss(q("(ros:quicklisp)"),ret);
   }
 
   if(program || script) {
-    char *tmp;
-    arg[paramc++]=q("--eval");
-    tmp=cat("(ros:run '(",program?program:"",script?"(:script ":"",script?script:"",script?")":"",script?"(:quit ())":"","))",NULL);
-    arg[paramc++]=tmp;
+    ret=conss(q("--eval"),ret);
+    ret=conss(cat("(ros:run '(",program?program:"",
+                      script?"(:script ":"",script?script:"",script?")":"",script?"(:quit ())":"",
+                      "))",NULL),ret);
   }
 
   for(i=1;i<argc;++i)
-    arg[paramc++]=argv[i];
+    ret=conss(q(argv[i]),ret);
 
   s(impl_path),s(binname);
 
-  arg[paramc]=NULL;
-  return arg;
+  return stringlist_array(nreverse(ret));
 }
