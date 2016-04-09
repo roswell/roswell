d3f49a9b0ac1f92da9341f082391e503c02f7aec
SANO Masatoshi <snmsts@gmail.com>
2016-04-09 15:39:42 +0900

    shorten cmd-run-acl.c

--- src/cmd-run-acl.c
+++ src/cmd-run-acl.c
@@ -9,8 +9,9 @@ char** cmd_run_acl(int argc,char** argv,struct sub_command* cmd) {
   char* impl=(char*)cmd->name;
   char* lisp=strcmp(impl,"mlisp")==0?"mlisp":"alisp";
   char* version=(char*)cmd->short_name;
-  int offset=8; /*[binpath for alisp] --qq -I param -e init.lisp
-                  [terminating NULL] that total 7 are default. */
+
+  /*[binpath for alisp] --qq -I param -e init.lisp
+    [terminating NULL] that total 7 are default. */
   int i;
   char* impl_path= cat(home,"impls",SLASH,arch,SLASH,os,SLASH,"alisp",SLASH,version,NULL);
   char* help=get_opt("help",0);
@@ -18,63 +19,49 @@ char** cmd_run_acl(int argc,char** argv,struct sub_command* cmd) {
   char* image=get_opt("image",0);
   char* program=get_opt("program",0);
   char* acl_version=get_opt("version",0);
-  int paramc=0;
-  char *bin;
-  int issystem=(strcmp("system",version)==0);
-  bin=issystem?truename(which(lisp)):cat(impl_path,SLASH,"bin",SLASH,lisp,EXE_EXTENTION,NULL);
+  LVal ret=0;
   s(arch),s(os);
-  if(acl_version)
-    offset+=3;
-  if(script)
-    offset+=1;
-  if(quicklisp)
-    offset+=2;
-  if(program||script)
-    offset+=2;
 
-  arg=alloc(sizeof(char*)*(offset+argc));
-  arg[paramc++]=bin;
+  ret=conss((strcmp("system",version)==0)?
+            truename(which(lisp)):
+            cat(impl_path,SLASH,"bin",SLASH,lisp,EXE_EXTENTION,NULL),ret);
   /* runtime options from here */
-  arg[paramc++]=q("-qq");
+  ret=conss(q("-qq"),ret);
   if(acl_version) {
-    arg[paramc++]=q("-e");
-    arg[paramc++]=q("(format t \"~A ~A~%\" (lisp-implementation-type) (lisp-implementation-version))");
-    arg[paramc++]=q("-kill");
+    ret=conss(q("-e"),ret);
+    ret=conss(q("(format t \"~A ~A~%\" (lisp-implementation-type) (lisp-implementation-version))"),ret);
+    ret=conss(q("-kill"),ret);arg[paramc++]=q("-kill");
   }
 
   if(image) {
     char *path=cat(impl_path,SLASH,"dump",SLASH,image,".core",NULL);
     if(file_exist_p(path)){
-      arg[paramc++]=q("-I");
-      arg[paramc++]=path;
-    }else {
+      ret=conss(q("-I"),ret);
+      ret=conss(path,ret);
+    }else 
       cond_printf(1,"core not found:%s\n",path);
-      arg[paramc++]=cat(impl_path,SLASH,"lib",SLASH,"sbcl",SLASH,"sbcl.core",NULL);
-      s(path);
-    }
+    s(path);
   }
-  arg[paramc++]=q("-e");
-  arg[paramc++]=s_cat(q("(progn(setq *load-verbose*()*compile-verbose*())#-ros.init(cl:load \""),s_escape_string(lispdir()),q("init.lisp"),q("\"))"),NULL);
+  ret=conss(q("-e"),ret);
+  ret=conss(s_cat(q("(progn(setq *load-verbose*()*compile-verbose*())#-ros.init(cl:load \""),
+                  s_escape_string(lispdir()),q("init.lisp"),q("\"))"),NULL),ret);
 
   if(quicklisp) {
-    arg[paramc++]=q("-e");
-    arg[paramc++]=q("(ros:quicklisp)");
+    ret=conss(q("-e"),ret);
+    ret=conss(q("(ros:quicklisp)"),ret);
   }
   if(program || script) {
-    char *tmp;
-    arg[paramc++]=q("-e");
-    tmp=cat("(ros:run '(",program?program:"",script?"(:script ":"",script?script:"",script?")":"",script?"(:quit ())":"","))",NULL);
-    arg[paramc++]=tmp;
+    ret=conss(q("-e"),ret);
+    ret=conss(cat("(ros:run '(",program?program:"",
+                  script?"(:script ":"",script?script:"",script?")":"",script?"(:quit ())":"",
+                  "))",NULL),ret);
   }
 
-  for(i=1;i<argc;++i) {
-    arg[paramc++]=argv[i];
-  }
+  for(i=1;i<argc;++i)
+    ret=conss(q(argv[i]),ret);
 
   s(impl_path);
 
-  arg[paramc]=NULL;
-  cond_printf(1,"\nhelp=%s script=%s\n",help?"t":"nil"
-              ,script?script:"nil");
-  return arg;
+  cond_printf(1,"\nhelp=%s script=%s\n",help?"t":"nil",script?script:"nil");
+  return stringlist_array(nreverse(ret));
 }
