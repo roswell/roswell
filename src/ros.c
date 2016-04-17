5b35f0082704c46912779d79ada514b74d12e2c1
SANO Masatoshi <snmsts@gmail.com>
2016-04-18 02:52:11 +0900

    separate ros.c into ros.c and register-commands.c

--- src/ros.c
+++ src/ros.c
@@ -6,12 +6,8 @@ int argc_orig;
 struct opts* global_opt;
 struct opts* local_opt=NULL;
 
-extern int cmd_internal(int argc,char **argv,struct sub_command* cmd);
-extern int opt_version(int argc,char **argv,struct sub_command* cmd);
-
-extern void register_cmd_run(void);
-extern void register_cmd_install(void);
-extern void register_cmd_internal(void);
+extern LVal register_runtime_options();
+extern LVal register_runtime_commands();
 
 int verbose=0;
 int testing=0;
@@ -134,101 +130,15 @@ int proccmd(int argc,char** argv,LVal option,LVal command) {
   return 1;
 }
 
-#define OPT_SETVAL(sym,rexp)                                    \
-  int opt_##sym(int argc,char** argv,struct sub_command* cmd) { \
-    sym=rexp;                                                   \
-    cond_printf(1,"opt:%s:%d\n",cmd->name,sym);                 \
-    return 1;}
-
-OPT_SETVAL(verbose,(strcmp(cmd->name,"verbose")==0)?1|verbose<<1:verbose>>1)
-OPT_SETVAL(testing,1+testing)
-OPT_SETVAL(rc,(strcmp(cmd->name,"rc")==0)?1:0)
-OPT_SETVAL(quicklisp,(strcmp(cmd->name,"quicklisp")==0)?1:0)
-
-int opt_program0(int argc,char** argv,struct sub_command* cmd) {
-  if(cmd->name) {
-    char* current=get_opt("program",0);
-    current=cat(current?current:"","(:",cmd->name,")",NULL);
-    set_opt(&local_opt,"program",current);
-  }
-  return 1;
-}
-
-int opt_take1(int argc,char** argv,struct sub_command* cmd) {
-  const char* arg=cmd->name;
-  if(arg && argc>1)
-    set_opt(&local_opt,arg,argv[1]);
-  return 2;
-}
-
-#define OPT_APPEND(sym)                                            \
-  int opt_##sym(int argc,char** argv,struct sub_command* cmd) {    \
-    if(cmd->name && argc>1) {                                      \
-      char* current=get_opt(#sym,0);                               \
-      current=s_cat(current?q(current):q(""),                      \
-                    q("(:"),q(cmd->name),q(" \""),                 \
-                    escape_string(argv[1]),q("\")"),NULL);         \
-      set_opt(&local_opt,#sym,current);                            \
-    }                                                              \
-    return 2;                                                      \
-  }
-
-OPT_APPEND(program)
-OPT_APPEND(restart)
-OPT_APPEND(final)
-
-LVal register_runtime_options() {
-  LVal opt=0;
-  opt=add_command(opt,"version" ,NULL,opt_version,1,1);
-  opt=add_command(opt,"wrap","-w",opt_take1,1,0);
-  opt=add_command(opt,"image","-m",opt_take1,1,0);
-  opt=add_command(opt,"lisp","-L",opt_take1,1,0);
-
-  /*opt=add_command(opt,"file","-f",opt_program,1,0,"include lisp FILE while building","FILE");*/
-  opt=add_command(opt,"load","-l",opt_program,1,0);
-  opt=add_command(opt,"source-registry","-S",opt_program,1,0);
-  opt=add_command(opt,"system","-s",opt_program,1,0);
-  opt=add_command(opt,"load-system",NULL,opt_program,1,0);
-  opt=add_command(opt,"package","-p",opt_program,1,0);
-  opt=add_command(opt,"system-package","-sp",opt_program,1,0);
-  opt=add_command(opt,"eval","-e",opt_program,1,0);
-  opt=add_command(opt,"require",NULL,opt_program,1,0);
-  opt=add_command(opt,"quit","-q",opt_program0,1,0);
-
-  opt=add_command(opt,"restart","-r",opt_restart,1,0);
-  opt=add_command(opt,"entry","-E",opt_restart,1,0);
-  opt=add_command(opt,"init","-i",opt_restart,1,0);
-  opt=add_command(opt,"print","-ip",opt_restart,1,0);
-  opt=add_command(opt,"write","-iw",opt_restart,1,0);
-
-  opt=add_command(opt,"final","-F",opt_final,1,0);
-
-  opt=add_command(opt,"rc","-R",opt_rc,1,0);
-  opt=add_command(opt,"no-rc","+R",opt_rc,1,0);
-  opt=add_command(opt,"quicklisp","-Q",opt_quicklisp,1,0);
-  opt=add_command(opt,"no-quicklisp","+Q",opt_quicklisp,1,0);
-  opt=add_command(opt,"verbose","-v",opt_verbose,1,0);
-  opt=add_command(opt,"quiet",NULL,opt_verbose,1,0);
-  opt=add_command(opt,"test",NULL,opt_testing,1,0);
-  opt=add_command(opt,"stdin",NULL,opt_program,0,0);
-  return nreverse(opt);
-}
-
 int main (int argc,char **argv) {
   int i;
+  char* path=s_cat(configdir(),q("config"),NULL);
   argv_orig=argv;
   argc_orig=argc;
 
   top_options=register_runtime_options();
+  top_commands=register_runtime_commands();
 
-  /*commands*/
-  register_cmd_install();
-  top_commands=add_command(top_commands,"roswell-internal-use",NULL,cmd_internal,0,1);
-  register_cmd_internal();
-  register_cmd_run();
-  top_commands=nreverse(top_commands);
-
-  char* path=s_cat(configdir(),q("config"),NULL);
   global_opt=load_opts(path);
   struct opts** opts=&global_opt;
   unset_opt(opts,"program");
