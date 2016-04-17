104fe3451dd0e89a67211d3ee26a1228cdcb1f92
SANO Masatoshi <snmsts@gmail.com>
2016-04-17 22:05:45 +0900

    separate util-dir.c

--- src/util-dir.c
+++ src/util-dir.c
@@ -1,27 +1,79 @@
 /* -*- tab-width : 2 -*- */
 #include "util.h"
 
-char* homedir(void) {
-  char *c=q_(PACKAGE"_HOME");
-  char *env=getenv(c);
-  s(c);
-  if(env)
-    return append_trail_slash(q(env));
-#ifdef HAVE_WINDOWS_H
-  TCHAR szAppData[MAX_PATH];
-  if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PROFILE, NULL, 0, szAppData)))
-    c=q_(szAppData);
-#else
+#ifndef HAVE_WINDOWS_H
+char* homedir_helper(void) {
   char* user=getenv("SUDO_USER");
   struct passwd *pwd= getpwuid(getuid());
   if(user)
     pwd=getpwnam(user);
-  if(pwd)
-    c=q_(pwd->pw_dir);
+  return pwd?q_(pwd->pw_dir):NULL;
+}
+
+char* truename(const char* path) {
+  char* ret=realpath(path,NULL);
+  return ret?ret:which((char*)path);
+}
+
+int ensure_directories_exist (char* path) {
+  int len = strlen(path);
+  cond_printf(1,"ensure_directories_exist:%s\n",path);
+  if(len) {
+    for(--len;(path[len]!=SLASH[0]||len==-1);--len);
+    path=subseq(path,0,len+1);
+  }else
+    path=q(path);
+  
+  if(!directory_exist_p(path)) {
+    pid_t  pid=fork();
+    if(pid==-1) {
+      perror("fork");
+      return 0;
+    }
+    if(pid==0) {
+      char* cmd=cat("mkdir -p ",path,NULL);
+      setup_uid(0);
+      if(System(cmd)!=0) {
+        fprintf(stderr,"failed:%s\n",cmd);
+        exit(1);
+      }
+      s(cmd);
+      exit(0);
+    }else {
+      int status;
+      s(path);
+      waitpid(pid,&status,0);
+      return WEXITSTATUS(status);
+    }
+    s(path);
+  }
+  return 1;
+}
+
+int directory_exist_p (char* path) {
+  struct stat sb;
+  return stat(path, &sb) == 0 && S_ISDIR(sb.st_mode)?1:0;
+}
+
+int change_directory(const char* path) {
+  return chdir(path);
+}
+
+int delete_directory(char* pathspec,int recursive) {
+  char* cmd=s_cat2(q(recursive?"rm -rf ":"rmdir "),q(pathspec));
+  int ret=System(cmd);
+  s(cmd);
+  return ret==0;
+}
+
 #endif
-  else
-    return NULL; /* error? */
-  return append_trail_slash(c);
+
+char* homedir(void) {
+  char *c=q_(PACKAGE"_HOME");
+  char *env=getenv(c);
+  s(c);
+  return env?append_trail_slash(q(env)):
+    ((c=homedir_helper())?append_trail_slash(c):NULL);
 }
 
 char* configdir(void) {
@@ -33,20 +85,6 @@ char* subcmddir(void) {
   return lispdir();
 }
 
-char* truename(const char* path) {
-#ifndef HAVE_WINDOWS_H
-  char* ret=realpath(path,NULL);
-  if(ret)
-    return ret;
-#else
-  char ret[MAX_PATH];
-  DWORD dwret;
-  dwret=GetFullPathName(path,sizeof(ret)/sizeof(ret[0]),ret,NULL);
-  if(dwret)
-    return q(ret);
-#endif
-  return which((char*)path);
-}
 
 char* pathname_directory(char* path) {
   int i;
@@ -64,87 +102,4 @@ char* file_namestring(char* path) {
   ret=(path[i]=='/')?subseq(path,i+1,0):q(path);
   s(path);
   return ret;
-}
-
-int ensure_directories_exist (char* path) {
-  int len = strlen(path);
-  cond_printf(1,"ensure_directories_exist:%s\n",path);
-  if(len) {
-    for(--len;(path[len]!=SLASH[0]||len==-1);--len);
-    path=subseq(path,0,len+1);
-  }else path=q(path);
-  if(!directory_exist_p(path)) {
-#ifndef HAVE_WINDOWS_H
-  pid_t  pid=fork();
-  if(pid==-1) {
-    perror("fork");
-    return 0;
-  }
-  if(pid==0) {
-    char* cmd=cat("mkdir -p ",path,NULL);
-    setup_uid(0);
-    if(System(cmd)!=0) {
-      fprintf(stderr,"failed:%s\n",cmd);
-      exit(1);
-    }
-    s(cmd);
-    exit(0);
-  }else {
-    int status;
-    s(path);
-    waitpid(pid,&status,0);
-    return WEXITSTATUS(status);
-  }
-#else
-    SHCreateDirectoryEx(NULL,path,NULL);
-#endif
-    s(path);
-  }
-  return 1;
-}
-
-int directory_exist_p (char* path) {
-#ifndef HAVE_WINDOWS_H
-  struct stat sb;
-  return stat(path, &sb) == 0 && S_ISDIR(sb.st_mode)?1:0;
-#else
-  WIN32_FIND_DATA fd;
-  char *p=cat(path,"*.*",NULL);
-  HANDLE dir=FindFirstFile(p,&fd);
-  s(p);
-  if(dir==INVALID_HANDLE_VALUE)
-    return 0;
-  FindClose(dir);
-  return 1;
-#endif
-}
-
-int change_directory(const char* path) {
-#ifndef _WIN32
-  return chdir(path);
-#else
-  return _chdir(path);
-#endif
-}
-
-int delete_directory(char* pathspec,int recursive) {
-#ifndef HAVE_WINDOWS_H
-  char* cmd=s_cat2(q(recursive?"rm -rf ":"rmdir "),q(pathspec));
-  int ret=System(cmd);
-  s(cmd);
-  return ret==0;
-#else
-  if(!recursive) {
-    return(!!RemoveDirectory(pathspec));
-  }else {
-    SHFILEOPSTRUCT fs;
-    ZeroMemory(&fs, sizeof(SHFILEOPSTRUCT));
-    fs.hwnd = NULL;
-    fs.wFunc = FO_DELETE;
-    fs.pFrom = pathspec;
-    fs.pTo = NULL;
-    fs.fFlags=FOF_SIMPLEPROGRESS|FOF_NOCONFIRMATION;
-    return (SHFileOperation(&fs) == 0);
-  }
-#endif  
 }
---
+++ src/util-dir_windows.c
@@ -0,0 +1,63 @@
+/* -*- tab-width : 2 -*- */
+#include "util.h"
+
+#ifdef HAVE_WINDOWS_H
+
+char* homedir_helper(void) {
+  TCHAR szAppData[MAX_PATH];
+  return (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PROFILE, NULL, 0, szAppData)))?q_(szAppData):NULL;
+}
+
+char* truename(const char* path) {
+  char ret[MAX_PATH];
+  DWORD dwret;
+  dwret=GetFullPathName(path,sizeof(ret)/sizeof(ret[0]),ret,NULL);
+  return dwret?q(ret):which((char*)path)
+}
+
+int ensure_directories_exist (char* path) {
+  int len = strlen(path);
+  cond_printf(1,"ensure_directories_exist:%s\n",path);
+  if(len) {
+    for(--len;(path[len]!=SLASH[0]||len==-1);--len);
+    path=subseq(path,0,len+1);
+  }else path=q(path);
+  if(!directory_exist_p(path)) {
+    SHCreateDirectoryEx(NULL,path,NULL);
+    s(path);
+  }
+  return 1;
+}
+
+int directory_exist_p (char* path) {
+  WIN32_FIND_DATA fd;
+  char *p=cat(path,"*.*",NULL);
+  HANDLE dir=FindFirstFile(p,&fd);
+  s(p);
+  if(dir==INVALID_HANDLE_VALUE)
+    return 0;
+  FindClose(dir);
+  return 1;
+}
+
+
+int change_directory(const char* path) {
+  return _chdir(path);
+}
+
+int delete_directory(char* pathspec,int recursive) {
+  if(!recursive) {
+    return(!!RemoveDirectory(pathspec));
+  }else {
+    SHFILEOPSTRUCT fs;
+    ZeroMemory(&fs, sizeof(SHFILEOPSTRUCT));
+    fs.hwnd = NULL;
+    fs.wFunc = FO_DELETE;
+    fs.pFrom = pathspec;
+    fs.pTo = NULL;
+    fs.fFlags=FOF_SIMPLEPROGRESS|FOF_NOCONFIRMATION;
+    return (SHFileOperation(&fs) == 0);
+  }
+}
+
+#endif
