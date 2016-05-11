bdd4d41050ac058bf0643fad3f71da7ae7feddf6
SANO Masatoshi <snmsts@gmail.com>
2016-05-12 00:23:50 +0900

    fix a bug.(comes from package difference)

--- lisp/install-quicklisp.lisp
+++ lisp/install-quicklisp.lisp
@@ -7,63 +7,64 @@
 (defun quicklisp-patch (path)
   (with-open-file (out (ensure-directories-exist (merge-pathnames "local-init/ros-download.lisp" path))
                        :direction :output :if-exists :supersede)
-    (format
-     out "~@{~s~^~%~}"
-     '(let ((*error-output* (make-broadcast-stream)))
-       (setf (fdefinition (find-symbol (string :fetch) :ql-http))
-        (lambda (url file &key (follow-redirects t) quietly
-                            (maximum-redirects 10))
-          "Request URL and write the body of the response to FILE."
-          (declare (ignorable url follow-redirects quietly maximum-redirects))
-          (ros:roswell `("roswell-internal-use" "download"
-                                                ,(funcall (find-symbol (string :urlstring) :ql-http)
-                                                          (funcall (find-symbol (string :url) :ql-http) url)) ,file "2")
-                       (if (find :abcl *features*)
-                           :interactive *standard-output*))
-          (values (make-instance (find-symbol (string :header) :ql-http) :status 200)
-                  (probe-file file)))))
-     '(pushnew :quicklisp-support-https *features*)
-     '(in-package #:ql-dist)
-     '(let ((*error-output* (make-broadcast-stream)))
-       (when (or (loop for k in '(:win32 :windows :mswindows)
-                       never (find k *features*))
-                 (probe-file (merge-pathnames (format nil "impls/~A/windows/7za/9.20/7za.exe"
-                                                      (ros:roswell '("roswell-internal-use""uname""-m") :string t))
-                                              (ros:opt "homedir"))))
-         (defmethod install ((release release))
-           (let ((archive (ensure-local-archive-file release))
-                 (output (relative-to (dist release)
-                                      (make-pathname :directory
-                                                     (list :relative "software"))))
-                 (tracking (install-metadata-file release)))
-             (ensure-directories-exist output)
-             (ensure-directories-exist tracking)
-             (ros:roswell `("roswell-internal-use" "tar" "-xf" ,archive "-C" ,output))
-             (ensure-directories-exist tracking)
-             (with-open-file (stream tracking
-                                     :direction :output
-                                     :if-exists :supersede)
-               (write-line (qenough (base-directory release)) stream))
-             (let ((provided (provided-systems release))
-                   (dist (dist release)))
-               (dolist (file (system-files release))
-                 (let ((system (find-system-in-dist (pathname-name file) dist)))
-                   (unless (member system provided)
-                     (error "FIND-SYSTEM-IN-DIST returned ~A but I expected one of ~A"
-                            system provided))
-                   (let ((system-tracking (install-metadata-file system))
-                         (system-file (merge-pathnames file
-                                                       (base-directory release))))
-                     (ensure-directories-exist system-tracking)
-                     (unless (probe-file system-file)
-                       (error "Release claims to have ~A, but I can't find it"
-                              system-file))
-                     (with-open-file (stream system-tracking
-                                             :direction :output
-                                             :if-exists :supersede)
-                       (write-line (qenough system-file)
-                                   stream))))))
-             release)))))))
+    (let ((*package* (find-package :ros.install)))
+      (format
+       out "~@{~s~^~%~}"
+       '(let ((*error-output* (make-broadcast-stream)))
+         (setf (fdefinition (find-symbol (string :fetch) :ql-http))
+          (lambda (url file &key (follow-redirects t) quietly
+                              (maximum-redirects 10))
+            "Request URL and write the body of the response to FILE."
+            (declare (ignorable url follow-redirects quietly maximum-redirects))
+            (ros:roswell `("roswell-internal-use" "download"
+                                                  ,(funcall (find-symbol (string :urlstring) :ql-http)
+                                                            (funcall (find-symbol (string :url) :ql-http) url)) ,file "2")
+                         (if (find :abcl *features*)
+                             :interactive *standard-output*))
+            (values (make-instance (find-symbol (string :header) :ql-http) :status 200)
+                    (probe-file file)))))
+       '(pushnew :quicklisp-support-https *features*)
+       '(in-package #:ql-dist)
+       '(let ((*error-output* (make-broadcast-stream)))
+         (when (or (loop for k in '(:win32 :windows :mswindows)
+                      never (find k *features*))
+                   (probe-file (merge-pathnames (format nil "impls/~A/windows/7za/9.20/7za.exe"
+                                                        (ros:roswell '("roswell-internal-use""uname""-m") :string t))
+                                                (ros:opt "homedir"))))
+           (defmethod install ((release release))
+             (let ((archive (ensure-local-archive-file release))
+                   (output (relative-to (dist release)
+                                        (make-pathname :directory
+                                                       (list :relative "software"))))
+                   (tracking (install-metadata-file release)))
+               (ensure-directories-exist output)
+               (ensure-directories-exist tracking)
+               (ros:roswell `("roswell-internal-use" "tar" "-xf" ,archive "-C" ,output))
+               (ensure-directories-exist tracking)
+               (with-open-file (stream tracking
+                                       :direction :output
+                                       :if-exists :supersede)
+                 (write-line (qenough (base-directory release)) stream))
+               (let ((provided (provided-systems release))
+                     (dist (dist release)))
+                 (dolist (file (system-files release))
+                   (let ((system (find-system-in-dist (pathname-name file) dist)))
+                     (unless (member system provided)
+                       (error "FIND-SYSTEM-IN-DIST returned ~A but I expected one of ~A"
+                              system provided))
+                     (let ((system-tracking (install-metadata-file system))
+                           (system-file (merge-pathnames file
+                                                         (base-directory release))))
+                       (ensure-directories-exist system-tracking)
+                       (unless (probe-file system-file)
+                         (error "Release claims to have ~A, but I can't find it"
+                                system-file))
+                       (with-open-file (stream system-tracking
+                                               :direction :output
+                                               :if-exists :supersede)
+                         (write-line (qenough system-file)
+                                     stream))))))
+               release))))))))
 
 (defun quicklisp-argv-parse (argv)
   (set-opt "download.uri" (format nil "~A~A" (quicklisp-uri) "quicklisp.lisp"))
