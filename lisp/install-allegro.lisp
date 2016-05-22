557efded0b5fa17d8b766591d6774355f5ddd720
SANO Masatoshi <snmsts@gmail.com>
2016-05-22 20:33:19 +0900

    forget to add install-allegro.lisp

---
+++ lisp/install-allegro.lisp
@@ -0,0 +1,89 @@
+(in-package :ros.install)
+(ros:quicklisp :environment nil)
+
+(defun allegro-get-version ()
+  (list "100express"))
+
+(defun allegro-version (argv)
+  (let ((version (getf argv :version)))
+    (when (or (null version) (equal version "latest"))
+      (setf (getf argv :version) (first (allegro-get-version)))))
+  (cons t argv))
+
+(defvar *allegro-uname-m-alist*
+  '(("x86-64" . "x86")))
+
+(defvar *allegro-uname-alist*
+  '(("linux" . "linu")
+    ("darwin" . "macos")))
+
+(defun allegro-uname-m ()
+  (or (cdr (assoc (uname-m) *allegro-uname-m-alist* :test 'equal))
+      (uname-m)))
+
+(defun allegro-uname ()
+  (or (cdr (assoc (uname) *allegro-uname-alist* :test 'equal))
+      (uname)))
+
+(defun allegro-argv-parse (argv)
+  (let ((uname (allegro-uname))
+        (uname-m (allegro-uname-m)))
+    (format *error-output* "~&Installing allegro/~A...~%" (getf argv :version))
+    (set-opt "as" (getf argv :version))
+    (set-opt "download.uri" (format nil "~@{~A~}" (allegro-uri)
+                                    "/acl" (getf argv :version) "/" uname uname-m "/acl" (getf argv :version)
+                                    "-" uname "x-" uname-m (cond ((equal uname "macos") ".dmg")
+                                                                 ((equal uname "linu") ".bz2"))))
+    (set-opt "download.archive" (let ((pos (position #\/ (get-opt "download.uri") :from-end t)))
+                                  (when pos 
+                                    (merge-pathnames (format nil "archives/~A" (subseq (get-opt "download.uri") (1+ pos))) (homedir)))))
+    (cons t argv)))
+
+(defun allegro-download (argv)
+  (if (or (not (probe-file (get-opt "download.archive")))
+          (get-opt "download.force"))
+      (progn
+        (format t "~&Downloading archive:~A~%" (get-opt "download.uri"))
+        (download (get-opt "download.uri") (get-opt "download.archive")))
+      (format t "~&Skip downloading ~A~%specify download.force=t to download it again.~%"
+              (get-opt "download.uri")))
+  (cons t argv))
+
+(defun allegro-expand (argv)
+  (format t "~%Extracting archive:~A~%" (get-opt "download.archive"))
+  (let* ((impls (merge-pathnames (format nil "impls/~A/~A/allegro/" (uname-m) (uname)) (homedir)))
+         (path (merge-pathnames (format nil "~A/" (get-opt "as")) impls)))
+    (expand (get-opt "download.archive") (ensure-directories-exist impls))
+    (and (probe-file path)
+         (uiop/filesystem:delete-directory-tree 
+          path :validate t))
+    (ql-impl-util:rename-directory
+     (merge-pathnames (format nil "acl~A/" (getf argv :version)) impls)
+     (merge-pathnames (format nil "~A/" (get-opt "as")) impls)))
+  (cons t argv))
+
+(defun allegro-help (argv)
+  (format t "allegro install options~%")
+  (flet ((fmt (param default more)
+           (format t "--~A~A ~A~%~5T~A~%"
+                   (cond ((eql default t) "without-")
+                         ((null default) "with-")
+                         (t ""))
+                   param
+                   (or (and (not (null default))
+                            (not (eql default t))
+                            default)
+                       "")
+                   more)))
+    (fmt "install" t "Download archive"))
+  (cons t argv))
+
+(push `("allegro" . (allegro-version
+                     allegro-argv-parse
+                     allegro-download
+                     allegro-expand
+                     #+nil setup))
+      *install-cmds*)
+
+(push `("allegro" . ,(list 'allegro-help)) *help-cmds*)
+(push `("allegro" . allegro-get-version) *list-cmd*)
