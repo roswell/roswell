a1e03fd6c19ca82f43416fd9ee4e58ac451c9446
SANO Masatoshi <snmsts@gmail.com>
2016-11-05 00:01:47 +0900

    edit system macro.

--- lisp/system.lisp
+++ lisp/system.lisp
@@ -6,12 +6,14 @@
 (ros:include "util")
 (in-package :ros.util)
 
-(defmacro system (file &key depends-on (prefix ":roswell.install"))
-  `(asdf:defsystem
-       ,(read-from-string
-         (format nil "~A.~A~A" prefix
-                 (subseq file (1+ (or (position #\- file)
-                                      (position #\+ file))))
-                 (if (find #\+ file) "+""")))
-     :components ((:file ,file))
-     ,@(when depends-on `(:depends-on ,depends-on))))
+(defmacro system (file &key depends-on (prefix ":roswell"))
+  (let ((pos (or (position #\- file)
+                 (position #\+ file))))
+    `(asdf:defsystem
+         ,(read-from-string
+           (format nil "~A.~A.~A~A" prefix
+                   (subseq file 0 pos)
+                   (subseq file (1+ pos))
+                   (if (find #\+ file) "+""")))
+       :components ((:file ,file))
+       ,@(when depends-on `(:depends-on ,depends-on)))))
