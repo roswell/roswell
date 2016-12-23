d4a6c955d8b9c941d32f9d2855a745ad306a0ab9
SANO Masatoshi <snmsts@gmail.com>
2016-12-23 14:21:54 +0900

    testing 'ros run'

--- lisp/run-msys.lisp
+++ lisp/run-msys.lisp
@@ -5,15 +5,16 @@
 
 (defvar *msys2-arch*)
 (defvar *msys2-bits*)
-(defun msys (system version program restart script verbose)
-  (declare (ignore system version program restart script verbose))
+(defun msys (script args)
+  (declare (ignorable script args))
   (let* ((*msys2-bits* (or ;;(and (position "--32" (getf argv :argv) :test 'equal) 32)
                         ;;(and (position "--64" (getf argv :argv) :test 'equal) 64)
                         #+x86-64 64
                         #-x86-64 32))
          (*msys2-arch* (if (= 32 *msys2-bits*)
                            "i686" "x86_64"))
-         (msys (merge-pathnames (format nil "impls/~A/~A/msys~A/" (uname-m) (uname) *msys2-bits*) (homedir))))
+         #+win32(msys (merge-pathnames (format nil "impls/~A/~A/msys~A/" (uname-m) (uname) *msys2-bits*) (homedir))))
     (config-env)
-    (uiop/run-program:run-program
-     `(,(sb-ext:native-namestring (merge-pathnames "msys2_shell.cmd" msys))))))
+    #+win32(uiop/run-program:run-program
+            `(,(sb-ext:native-namestring (merge-pathnames "msys2_shell.cmd" msys))))
+    #-win32(print (list script args (ros::ros-opts)))))
