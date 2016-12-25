6a1034f3fdd57ab2197e76b8323d9f3381937353
SANO Masatoshi <snmsts@gmail.com>
2016-12-25 20:01:13 +0900

    'ros init' use 'module' function.

---
+++ lisp/init-default.lisp
@@ -0,0 +1,42 @@
+(defpackage :roswell.init.default
+  (:use :cl :ros :roswell.util))
+(in-package :roswell.init.default)
+
+(defun default (name &rest params)
+  (declare (ignore name))
+  (let* ((date (get-universal-time))
+         (name (first params))
+         (path (make-pathname :defaults name :type "ros")))
+    (handler-case
+        (unless
+            (prog1
+                (with-open-file (out path
+                                     :direction :output
+                                     :if-exists nil
+                                     :if-does-not-exist :create)
+                  (when out
+                    (format out "~@{~A~%~}"
+                            "#!/bin/sh"
+                            "#|-*- mode:lisp -*-|#"
+                            "#| <Put a one-line description here>"
+                            "exec ros -Q -- $0 \"$@\"" "|#"
+                            "(progn ;;init forms"
+                            "  (ros:ensure-asdf)"
+                            "  ;;#+quicklisp (ql:quickload '() :silent t)"
+                            "  )"
+                            ""
+                            (format nil "(defpackage :ros.script.~A.~A" name date)
+                            "  (:use :cl))"
+                            (format nil "(in-package :ros.script.~A.~A)" name date)
+                            ""
+                            "(defun main (&rest argv)"
+                            "  (declare (ignorable argv)))"
+                            ";;; vim: set ft=lisp lisp:")
+                    (format t "~&Successfully generated: ~A~%" path)
+                    t))
+              #+sbcl (sb-posix:chmod path #o700))
+          (format *error-output* "~&File already exists: ~A~%" path)
+          (quit 1))
+      (error (e)
+        (format *error-output* "~&~A~%" e)
+        (quit 1)))))
