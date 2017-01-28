84ff09166470792adca6582f498c8b700dca0aa6
SANO Masatoshi <snmsts@gmail.com>
2017-01-28 23:05:25 +0900

    edit 'ros help run'

--- lisp/help-run.lisp
+++ lisp/help-run.lisp
@@ -6,6 +6,6 @@
 (defun run (argv)
   (declare (ignorable argv))
   (let ((s *error-output*))
-    (format s "Usage: ~A [OPTIONS] run [OPTIONS] [-- implementation-native-options...]~%~%" (opt "wargv0"))
+    (format s "Usage: ~A [OPTIONS] run [OPTIONS] [-- implementation-native-options...]~%~%" (pathname-name (opt "wargv0")))
     (let ((*options* (nthcdr 4 *options*))) ;; fix me
       (options nil))))
