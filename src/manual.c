991e0ba1ce55dd0f5759c27aea93f4c49d7acbb5
SANO Masatoshi <snmsts@gmail.com>
2017-05-14 15:17:40 +0900

    invoke man via abstruction layer.

--- src/manual.c
+++ src/manual.c
@@ -2,7 +2,10 @@
 #include "opt.h"
 
 DEF_SUBCMD(cmd_man) {
-  int argc=length(arg_);
-  cond_printf(1,"cmd_man:%d\n",argc);
-  return (0);
+  cond_printf(1,"cmd_man:%d\n",length(arg_));
+  char* man= which("man");
+  if(strlen(man))
+    exec_arg(stringlist_array(conss(man,rest(arg_))));
+  return 1;
 }
+
