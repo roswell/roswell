958e2d75ed336039a952a392b13c609783f4edd9
SANO Masatoshi <snmsts@gmail.com>
2017-06-08 15:03:36 +0900

    shorten dump.ros

--- lisp/dump-sbcl.lisp
+++ lisp/dump-sbcl.lisp
@@ -28,3 +28,37 @@
      (apply 'dump-executable args))
     (:output
      (sb-ext:save-lisp-and-die (first args)))))
+
+(in-package :ros.script.dump)
+
+(defun delete-compiler-information-sbcl ()
+  "This removes the entire compiler information about the functions.
+This includes macro/compiler-macro definitions, inline expansions, 
+IR1 (deftransform), IR2 (VOP) information in the infodb."
+  ;; see src/compiler/globaldb.lisp
+  #-sbcl
+  (warn "delete-compiler is available only in SBCL")
+  #+sbcl
+  (do-all-symbols (s)
+    (when (fboundp s)
+      (setf (sb-int:info :function :inlinep s) :notinline)
+      (sb-int:clear-info :function :inline-expansion-designator s)
+      ;; Does this have the same effect as proclaiming notinline?
+      ;; --- seems like so. src/compiler/proclaim.lisp
+      ;; --- SB-C::PROCESS-INLINE-DECLARATION
+      (sb-int:clear-info :function :source-transform s)
+      (sb-int:clear-info :function :info s)
+      (sb-int:clear-info :function :ir1-convert s)
+      (sb-int:clear-info :function :predicate-truth-constraint s)
+      (sb-int:clear-info :function :macro-function s)
+      (sb-int:clear-info :function :compiler-macro-function s))
+    (let ((s `(setf ,s)))
+      (when (fboundp s)
+        (setf (sb-int:info :function :inlinep s) :notinline)
+        (sb-int:clear-info :function :inline-expansion-designator s)
+        (sb-int:clear-info :function :source-transform s)
+        (sb-int:clear-info :function :info s)
+        (sb-int:clear-info :function :ir1-convert s)
+        (sb-int:clear-info :function :predicate-truth-constraint s)
+        (sb-int:clear-info :function :macro-function s)
+        (sb-int:clear-info :function :compiler-macro-function s)))))
