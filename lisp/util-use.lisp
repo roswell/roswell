fee972f44e6e0db72a3f5a0ad5c7d7968e113e5e
SANO Masatoshi <snmsts@gmail.com>
2017-06-20 17:44:39 +0900

    ros use extensible.

--- lisp/util-use.lisp
+++ lisp/util-use.lisp
@@ -4,19 +4,21 @@
   (:export :use))
 (in-package :roswell.util.use)
 
-(defun use (arg)
+(defun use (impl &rest args)
   "Parse the lisp version string (such as ccl-bin/1.11) and set it to the correct config slot(s)"
-  (destructuring-bind (lisp version) (parse-version-spec arg)
+  (destructuring-bind (lisp version) (parse-version-spec impl)
     (cond
-      ((and arg
+      ((let (func (module "use" lisp))
+         (when func
+           (apply func lisp version args))
+         func))
+      ((and impl
             (ignore-errors
-             (roswell:roswell `("-L" ,arg "version=t" "run"))))
+             (roswell:roswell `("-L" ,impl "version=t" "run"))))
        (cond ((and lisp version)
               (setf (config "default.lisp") lisp
                     (config (format nil "~A.version" lisp)) version))
              (lisp
               (setf (config "default.lisp")
                     lisp)))
-       t)
-      (t (let ((func (module "use" lisp)))
-           (funcall (or func 'identity) version))))))
+       t))))
