7c0d4e3603a706f8b6bccff2a737a0b8b5ff71c4
SANO Masatoshi <snmsts@gmail.com>
2017-06-22 15:32:58 +0900

    show installed versions when 'ros use asdf'

--- lisp/use-asdf.lisp
+++ lisp/use-asdf.lisp
@@ -10,7 +10,24 @@
            (asdf (config "asdf.version")))
        (format s (if (zerop (length asdf))
                      "ASDF version is not specified.~%"
-                     "choosen version is ~S~%") asdf))
+                     "choosen version is ~S~%") asdf)
+       (format s "Version Candidates:~%no~%~{~A~%~}"
+               (sort (mapcar (lambda (x) (car (last (pathname-directory x))) )
+                             (directory (merge-pathnames (format nil "lisp/asdf/*/") (homedir))))
+                     (lambda (a b)
+                       (loop for a- = a then (cdr a-)
+                             for b- = b then (cdr b-)
+                             for x = (car a-) for y = (car b-)
+                             do (cond ((null x) (return nil))
+                                      ((null y) (return t))
+                                      ((> x y) (return t))
+                                      ((< x y) (return nil)))))
+                     :key (lambda (x)
+                            (loop
+                              for p = (position #\. x)
+                              collect (parse-integer (subseq x 0 p))
+                              while p
+                              do (setq x (subseq x (1+ p))))))))
      t)
     ((member version '("no" "-") :test 'equal)
      (setf (config "asdf.version") nil)
