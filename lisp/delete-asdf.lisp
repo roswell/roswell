bb49afb4b7847cbafcefd02527572850feb97739
SANO Masatoshi <snmsts@gmail.com>
2017-06-22 10:37:46 +0900

    stub for 'ros delete asdf'

---
+++ lisp/delete-asdf.lisp
@@ -0,0 +1,7 @@
+(defpackage :roswell.delete.asdf
+  (:use :cl :roswell.util))
+(in-package :roswell.delete.asdf)
+
+(defun asdf (&rest argv)
+  (declare (ignore argv))
+  (format t "not yet implemented. TBD!~%"))
