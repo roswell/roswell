18e81f3e99072e2478163d031f9a8211665dac26
SANO Masatoshi <snmsts@gmail.com>
2017-07-26 00:23:37 +0900

    separate config.ros

---
+++ lisp/util-config.lisp
@@ -0,0 +1,34 @@
+(roswell:include "util" "util-config")
+(defpackage :roswell.util.config
+  (:use :cl)
+  (:export :load-config :save-config :unconfig :config))
+(in-package :roswell.util.config)
+
+(defun load-config (path)
+  (with-open-file (in path)
+    (loop for line = (read-line in nil nil)
+          while line
+          collect
+          (loop for last = -1 then pos
+                for pos = (position #\Tab line :start (1+ last))
+                collect (subseq line (1+ last) pos)
+                while pos))))
+
+(defun save-config (path config)
+  (with-open-file (out path :direction :output
+                            :if-exists :supersede
+                            :if-does-not-exist :create)
+    (loop for line in config
+          do (format out #.(format nil "~{~A~}" '("~{~A~^" #\Tab "~}~%")) line))))
+
+(defun unconfig (config var)
+  (remove var config :test 'equal :key #'first))
+
+(defun config (config var val)
+  (let ((found (find var config :test 'equal :key #'first)))
+    (if found
+        (progn
+          (setf (third found) val)
+          config)
+        (cons (list var "0" val)
+              config))))
