0974e20c6c31e764693f2671fcb47ed5ced4b205
SANO Masatoshi <snmsts@gmail.com>
2017-09-11 15:19:42 +0900

    add util-main.lisp

---
+++ lisp/util-main.lisp
@@ -0,0 +1,29 @@
+(roswell:include "util" "util-main")
+(defpackage :roswell.util.main
+  (:use :cl :roswell.util)
+  (:export :module-main))
+(in-package :roswell.util.main)
+
+(defun module-main (args &key default usage)
+  (let* ((name (first args))
+         (packagename (package-name *package*))
+         (pos (loop repeat 3
+                 with pos = 0
+                 for p = (position #\. packagename :start pos)
+                 while p
+                 collect p
+                 do (setf pos (1+ p))))
+         (module-name (string-downcase
+                       (subseq packagename (1+ (second pos)) (third pos)))))
+    (cond
+      ((and name
+            (module module-name name))
+       (apply (module module-name name) args))
+      ((and name
+            default
+            (module module-name default))
+       (apply (module module-name default) args))
+      ((and usage
+            (module module-name usage))
+       (apply (module module-name usage) args)))))
+
