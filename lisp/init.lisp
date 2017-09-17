ee80ddccc9222440146fa9c99384af33ec6829c9
SANO Masatoshi <snmsts@gmail.com>
2017-09-18 04:45:29 +0900

    local-projects to support env

--- lisp/init.lisp
+++ lisp/init.lisp
@@ -180,7 +180,9 @@ have the latest asdf, and this file has a workaround for this.
         (cl:load path :verbose (verbose))
         (loop with symbol = (read-from-string "ql:*local-project-directories*")
               for path in `(,local ,@(and (or path (and environment (getenv environment)))
-                                          `(,(merge-pathnames "local-projects/" (opt "homedir")))))
+                                          `(,(merge-pathnames (format nil "env/~A/local-projects/" (opt "roswellenv"))
+                                                              (opt "homedir"))
+                                             ,(merge-pathnames "local-projects/" (opt "homedir")))))
               for probe = (and path (or (ignore-errors (probe-file path))
                                         #+clisp(ext:probe-directory path)))
               when probe
