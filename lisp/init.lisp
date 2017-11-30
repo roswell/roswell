40f7f90058be34be865ab80dcf7154ea35bb860e
Masataro Asai <guicho2.71828@gmail.com>
2017-11-30 14:12:12 +0900

    doc

--- lisp/init.lisp
+++ lisp/init.lisp
@@ -175,7 +175,7 @@ have the latest asdf, and this file has a workaround for this.
            ;; which is <directory containing setup.lisp>/local-projects/ .
            for path in `(;; Searches local-project/ in the current directory
                          ,local
-                         ;; This is used when setup.lisp is found in a non-standard place, e.g. by the environment variable QUICKLISP_HOME
+                         ;; This is WHAAAAAT????
                          ,(ignore-errors
                             (truename (merge-pathnames "../../../local-projects/" (first (symbol-value symbol)))))
                          ;; Searches local-project/ in e.g. ~/.roswell/
