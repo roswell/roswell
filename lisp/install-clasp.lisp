e9e87498c9b6a57bc2c3c417e4ff86c99d94261d
gos-k <mag4.elan@gmail.com>
2017-12-20 17:15:08 +0900

    Updated clasp 2017-12-20

--- lisp/install-clasp.lisp
+++ lisp/install-clasp.lisp
@@ -5,7 +5,8 @@
 
 (defparameter *clasp-version*
   ;; alias commit external-clasp-version
-  '(("2017-12-12" "141be6a01d806efc64725e516dce8a58d3d1f732" "5.0")
+  '(("2017-12-20" "46634ed2f4dd927059e9d3ab00a00aee1f8991e3" "5.0")
+    ("2017-12-12" "141be6a01d806efc64725e516dce8a58d3d1f732" "5.0")
     ("2017-11-20" "97f9f147a35d9b18d9581bcd7c816aa45aecd894" "5.0")
     ("2017-11-05" "fb57b58cb27527469a0798ae5645e9e87bad7de4" "5.0")
     ("2017-10-27" "c4c0e740dba84dc3cdf4fd3dfa88404cb05f1932" "5.0")
