50df899c5bd796c34649894ba770207ff986ed81
SANO Masatoshi <snmsts@gmail.com>
2018-01-13 19:18:49 +0900

    wip 'ros dist'

---
+++ lisp/dist-list.lisp
@@ -0,0 +1,17 @@
+(defpackage :roswell.dist.list
+  (:use :cl)
+  (:shadow :list))
+(in-package :roswell.dist.list)
+
+(defun list (&rest r)
+  (if (second r)
+      (dolist (system (ql:provided-systems
+                       (ql-dist:find-dist (second r))))
+        (format t "~A~%" (ql-dist:short-description system)))
+      (dolist (dist (ql-dist:all-dists))
+        (format t "~A~15A~11A~A~%"
+                (if (ql-dist:enabledp dist) " " "#")
+                (ql-dist:name dist)
+                (subseq (ql-dist:version dist)
+                        0 (min (length (ql-dist:version dist)) 10))
+                (ql-dist::distinfo-subscription-url dist)))))
