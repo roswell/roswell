b5f86f12d605bc509d6c477296455b5724368423
SANO Masatoshi <snmsts@gmail.com>
2018-04-23 14:46:10 +0900

    uname -> uname_s

--- src/archive_windows.c
+++ src/archive_windows.c
@@ -5,7 +5,7 @@
 char* extract_command_str(int flags,const char *filename,int do_extract,const char* outputpath,char* type) {
   char* str;
   char* _uname_m=uname_m();
-  char* _uname=uname();
+  char* _uname=uname_s();
   char* _confdir=configdir();
   char* exe=s_escape_string(cat(_confdir,"impls",SLASH,_uname_m,SLASH,_uname,SLASH,"7za",SLASH,"9.20",SLASH,"7za.exe",NULL));
   char *outputpath2=q(outputpath);
