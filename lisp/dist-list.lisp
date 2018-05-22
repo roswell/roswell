8a55700cd731b0dd41774136bee95ef306590e45
SANO Masatoshi <snmsts@gmail.com>
2018-05-22 17:48:43 +0900

    sort dists

--- lisp/dist-list.lisp
+++ lisp/dist-list.lisp
@@ -8,7 +8,7 @@
       (dolist (system (ql:provided-systems
                        (ql-dist:find-dist (second r))))
         (format t "~A~%" (ql-dist:short-description system)))
-      (dolist (dist (ql-dist:all-dists))
+      (dolist (dist (sort (ql-dist:all-dists) #'> :key #'ql-dist:preference))
         (format t "~A~15A~11A~A~%"
                 (if (ql-dist:enabledp dist) " " "#")
                 (ql-dist:name dist)
