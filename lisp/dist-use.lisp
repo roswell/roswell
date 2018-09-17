a11ca9b8b2398521c35f58d04f16df661763c200
SANO Masatoshi <snmsts@gmail.com>
2018-09-17 15:34:25 +0900

    show usage on 'ros dist.'

--- lisp/dist-use.lisp
+++ lisp/dist-use.lisp
@@ -4,10 +4,12 @@
 
 (defun use (&rest r)
   (setf r (cdr r))
-  (let* ((name (first r))
-         (version (second r))
-         (alist (ql-dist:available-versions (ql-dist:dist name)))
-         (found (cdr (assoc version alist :test 'equal))))
-    (if found 
-        (ql-dist:install-dist found :replace t)
-        (format t "~A not found.~%" version))))
+  (if r
+      (let* ((name (first r))
+             (version (second r))
+             (alist (ql-dist:available-versions (ql-dist:dist name)))
+             (found (cdr (assoc version alist :test 'equal))))
+        (if found
+            (ql-dist:install-dist found :replace t)
+            (format t "~A not found.~%" version)))
+      (format t "usage: ros dist dist-name version~%")))
