439cf76abbfe940f6b34661b95d8061111b6b6ea
Jingtao Xu <jingtaozf@gmail.com>
2019-04-19 08:52:47 +0800

    use correct lispworks command line argument -eval

--- src/cmd-run-lispworks.c
+++ src/cmd-run-lispworks.c
@@ -15,14 +15,14 @@ char** cmd_run_lispworks(int argc,char** argv,struct sub_command* cmd) {
   ret=conss(cat(home,"impls",SLASH,arch,SLASH,os,SLASH,"LispWorks",SLASH,"lw-console",NULL), ret);
 
   if(lw_version) {
-    ret=conss(q("--eval"),ret);
+    ret=conss(q("-eval"),ret);
     ret=conss(q("(progn (format t \"~A ~A~%\" (lisp-implementation-type) (lisp-implementation-version))(lw:quit))"),ret);
   }
 
-  ret=conss(q("--eval"),ret);
+  ret=conss(q("-eval"),ret);
   ret=conss(s_cat(q("(progn #-ros.init(cl:load \""),s_escape_string(lispdir()),q("init.lisp"),q("\"))"),NULL),ret);
   if(program || script) {
-    ret=conss(q("--eval"),ret);
+    ret=conss(q("-eval"),ret);
     ret=conss(s_cat(q("(ros:run '("),q(program?program:""),
                     script?cat("(:script ",script,")","(:quit ())",NULL):q(""),
                     q("))"),NULL),ret);
