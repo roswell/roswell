7d38dc369dca205f259eb7ed856c2d3b914024cf
SANO Masatoshi <snmsts@gmail.com>
2019-05-07 09:21:16 +0000

    fixed?

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -1,13 +1,23 @@
 #!/bin/sh -ex
-deploy=linuxdeploy-x86_64.AppImage
+cd `dirname $0`
+cd ..
+deploy=appimagetool-x86_64.AppImage
 desktop=roswell.desktop
+appdir=roswell.AppDir
 icon=roswell.png
+./configure prefix=`pwd`/$appdir
+make
+make install
 # Download the toolchain
-curl -fsSLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/$deploy"
+curl -fsSLO "https://github.com/AppImage/AppImageKit/releases/download/11/$deploy"
 # Make it executable
 chmod +x "./$deploy"
+cd $appdir
 # Download the icon
 curl -fsSL "https://avatars0.githubusercontent.com/u/16501222?s=256&v=4" -o "$icon"
+curl -fsSL "https://raw.githubusercontent.com/AppImage/AppImageKit/master/resources/AppRun" -o AppRun
+chmod +x AppRun
+
 # Build appimage
 cat <<EOF > "$desktop"
 [Desktop Entry]
@@ -22,12 +32,5 @@ Icon=roswell
 Terminal=true
 MimeType=image/png;
 EOF
-appdir=`mktemp -d`
-./$deploy \
-    --icon-file="$icon" \
-    --desktop-file="roswell.desktop" \
-    --appdir="$appdir" \
-    --executable="$1" \
-    --output=appimage
-rm -rf $appdir $deploy $desktop
-
+cd ..
+./$deploy roswell.AppDir
