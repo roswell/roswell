686e0eed9b9bf55ffdda3ba06112464591f1f69f
SANO Masatoshi <snmsts@gmail.com>
2019-05-23 15:29:07 +0800

    prepare new update method.

---
+++ lisp/update-default.lisp
@@ -0,0 +1,8 @@
+(roswell:include "util")
+(defpackage :roswell.update.default
+  (:use :cl :roswell.util))
+(in-package :roswell.update.default)
+
+(defun default (&rest args)
+  (declare (ignore args))
+  )
