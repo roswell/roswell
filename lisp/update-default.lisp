6e3b0cfa926da2bf058d62ef9aebf75b9dabfe2d
SANO Masatoshi <snmsts@gmail.com>
2019-05-24 12:45:31 +0800

    update update-default

--- lisp/update-default.lisp
+++ lisp/update-default.lisp
@@ -3,6 +3,9 @@
   (:use :cl :roswell.util))
 (in-package :roswell.update.default)
 
-(defun default (&rest args)
+(defun default (param &rest args)
   (declare (ignore args))
-  )
+  (let* ((subpath (uiop:subpathp param (first ql:*local-project-directories*)))
+         (target (when subpath
+                   (format nil "~{~A~^/~}" (remove-if-not #'stringp (pathname-directory subpath))))))
+    (format *error-output* "Target project would be ~A~%" target)))
