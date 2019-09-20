56e6a6c6a3e188bbb270405e47bdcc4c8b6fff9b
SANO Masatoshi <snmsts@gmail.com>
2019-09-20 19:41:22 +0800

    tested 'ros -v uri=http://prdownloads.sourceforge.net/sbcl/sbcl-1.5.5-x86-64-linux-binary.tar.bz2 install sbcl-bin' to install sbcl-bin from external-uri

--- src/install-sbcl-bin.c
+++ src/install-sbcl-bin.c
@@ -35,9 +35,10 @@ void sbcl_bin_check_file(char* file) {
 int sbcl_version_bin(struct install_options* param) {
   char* home=configdir();
   char* platforms_html=cat(home,"tmp",SLASH,"sbcl-bin.html",NULL);
+  char* uri=get_opt("uri",0);
   cond_printf(1,"sbcl_version_bin\n");
   ensure_directories_exist(platforms_html);
-  param->version_not_specified=param->version?0:1;
+  param->version_not_specified=param->version||uri?0:1;
   if(param->version_not_specified) {
     int ret;
     printf("No SBCL version specified. Downloading platform-table.html to see the available versions...\n");
@@ -53,8 +54,23 @@ int sbcl_version_bin(struct install_options* param) {
     }
     sbcl_bin_check_file(platforms_html);
     param->version=sbcl_bin(platforms_html,param->version_not_specified++);
-  }else
-    param->version=q(param->version);
+  }else {
+    if(param->version)
+      param->version=q(param->version);
+    else {
+      char* name=file_namestring(q(uri));
+      int i;
+      for(i=0;name[i]!='-' && name[i]!='\0';++i);
+      if(name[i]=='-') {
+        param->version=q(name+i+1);
+        for(i=0;param->version[i]!='-' && param->version[i]!='\0';++i);
+        param->version[i]='\0';
+      }else {
+        param->version="unknown";
+      }
+      s(name);
+    }
+  }
   printf("Installing sbcl-bin/%s...\n",param->version);
   param->arch_in_archive_name=1;
 
@@ -66,13 +82,13 @@ int sbcl_bin_download(struct install_options* param) {
   int result;
   char* home=configdir();
   char* arch=arch_(param);
-  char* uri=get_opt("sbcl-bin-uri",0);
+  char* uri=get_opt("uri",0);
   cond_printf(1,"sbcl_bin_download\n");
   int retry=10;
   do {
     param->expand_path=cat(home,"src",SLASH,"sbcl","-",param->version,"-",arch,SLASH,NULL);
-    impls_sbcl_bin.uri=cat(uri?uri:SBCL_BIN_URI ,param->version,"/sbcl-",param->version,
-                           "-",arch,"-binary",sbcl_bin_extention(param),NULL);
+    impls_sbcl_bin.uri=uri?q(uri):cat(SBCL_BIN_URI ,param->version,"/sbcl-",param->version,
+                                   "-",arch,"-binary",sbcl_bin_extention(param),NULL);
     result = download(param);
     if(!result && param->version_not_specified) {
       int len = strlen(param->version)-1;
