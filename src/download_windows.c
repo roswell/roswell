354b9565011e64fc26f5a72c6732a3e4e9c6a943
SANO Masatoshi <snmsts@gmail.com>
2019-12-21 01:10:31 +0800

    implement head with wininet

--- src/download_windows.c
+++ src/download_windows.c
@@ -87,7 +87,45 @@ int download_simple (char* uri,char* path,int opt) {
 }
 
 int download_head (char* uri,int opt) {
-  // not supporeted yet.
+  URL_COMPONENTS u;
+  TCHAR szHostName[4096];
+  TCHAR szUrlPath[4096];
+  u.dwStructSize = sizeof(u);
+  u.dwSchemeLength    = 1;
+  u.dwHostNameLength  = 4096;
+  u.dwUserNameLength  = 1;
+  u.dwPasswordLength  = 1;
+  u.dwUrlPathLength   = 4096;
+  u.dwExtraInfoLength = 1;
+
+  u.lpszScheme     = NULL;
+  u.lpszHostName   = szHostName;
+  u.lpszUserName   = NULL;
+  u.lpszPassword   = NULL;
+  u.lpszUrlPath    = szUrlPath;
+  u.lpszExtraInfo  = NULL;
+  if(!InternetCrackUrl(uri,(DWORD)strlen(uri),0,&u)) {
+    return 4;
+  }
+  HINTERNET hSession = InternetOpen("WinInet",INTERNET_OPEN_TYPE_PRECONFIG,NULL,NULL,0);
+  HINTERNET hConnection = InternetConnect(hSession,szHostName,u.nPort,NULL,NULL,INTERNET_SERVICE_HTTP,0,0);
+  DWORD dwFlags = INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE;
+  if(INTERNET_SCHEME_HTTP == u.nScheme) {
+  }else if( INTERNET_SCHEME_HTTPS == u.nScheme )
+    dwFlags = dwFlags | INTERNET_FLAG_SECURE| INTERNET_FLAG_IGNORE_CERT_DATE_INVALID| INTERNET_FLAG_IGNORE_CERT_CN_INVALID;
+  else
+    return 3;
+  HINTERNET hRequest = HttpOpenRequest(hConnection,"HEAD",szUrlPath,NULL,NULL,NULL,dwFlags,0);
+
+  HttpSendRequest(hRequest,NULL,0,NULL,0);
+  DWORD dwStatusCode,dwContentLen;
+  DWORD dwLength = sizeof(DWORD);
+  if(HttpQueryInfo(hRequest,HTTP_QUERY_CONTENT_LENGTH | HTTP_QUERY_FLAG_NUMBER,&dwContentLen,&dwLength,0))
+    content_length=dwContentLen;
+  if(!HttpQueryInfo(hRequest,HTTP_QUERY_STATUS_CODE|HTTP_QUERY_FLAG_NUMBER,&dwStatusCode,&dwLength,0))
+    return 6;
+  if(HTTP_STATUS_OK != dwStatusCode)
+    return 5;
   return 0;
 }
 #endif
