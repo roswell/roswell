0321395fdbe82b2c764dd550c6fdeed4a45b80bb
SANO Masatoshi <snmsts@gmail.com>
2020-12-27 01:36:37 +0900

    ros serve swank --interface 0.0.0.0 should work.

--- lisp/serve-swank.lisp
+++ lisp/serve-swank.lisp
@@ -5,9 +5,15 @@
 
 (defun swank (&rest r)
   (ql:quickload :swank :silent t)
-  (let ((port 4005))
+  (let ((port 4005)
+        (interface "localhost"))
     (loop for (var val) on (cdr r) by #'cddr
           do (cond ((equal var "--port")
-                    (setf port (parse-integer val)))))
-    (roswell.util:read-call "swank:create-server" :port port :dont-close t))
+                    (setf port (parse-integer val)))
+                   ((equal var "--interface")
+                    (setf interface val))))
+    (roswell.util:read-call "swank:create-server"
+                            :interface interface
+                            :port port
+                            :dont-close t))
   (loop (sleep 1)))
