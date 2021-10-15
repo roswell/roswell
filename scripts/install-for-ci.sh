8fc2c35ac405c82337db6d7d44a7738334ec7e51
SANO Masatoshi <snmsts@gmail.com>
2021-10-15 20:12:36 +0900

    bump install-for-ci.sh version to 21.10.14.111

--- scripts/install-for-ci.sh
+++ scripts/install-for-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-ROSWELL_RELEASE_VERSION=21.06.14.110
+ROSWELL_RELEASE_VERSION=21.10.14.111
 ROSWELL_TARBALL_PATH=$HOME/roswell.tar.gz
 ROSWELL_DIR=$HOME/.roswell
 ROSWELL_REPO=${ROSWELL_REPO:-https://github.com/roswell/roswell}
