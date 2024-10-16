7d4262e76fde857151bb5d1c3dd13fc39d744d8c
snmsts <snmsts@gmail.com>
2024-10-16 15:51:12 +0900

    instant fix for failure of ci

--- scripts/install-for-ci.sh
+++ scripts/install-for-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-ROSWELL_RELEASE_VERSION=24.10.115
+ROSWELL_RELEASE_VERSION=23.10.14.114
 ROSWELL_TARBALL_PATH=$HOME/roswell.tar.gz
 ROSWELL_DIR=$HOME/.roswell
 ROSWELL_REPO=${ROSWELL_REPO:-https://github.com/roswell/roswell}
